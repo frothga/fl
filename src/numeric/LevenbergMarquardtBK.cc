c729afda6ece57988b9d0785d63fc4784cce22d4
rothgang <none@none>
2003-07-08 23:35:23 -0600

    Sparsified.

--- src/numeric/LevenbergMarquardtBK.cc
+++ src/numeric/LevenbergMarquardtBK.cc
@@ -1,10 +1,3 @@
-// This is a snapshot of the code just after converting to triangular
-// factorization (rather than QR), but before sparsification.  It worked
-// perfectly, at least for small problems, and the numbers from various parts
-// of the algorithm exactly matched those output by the C-ified but unmodified
-// LM.
-
-
 #include "fl/search.h"
 
 #include <float.h>
@@ -20,24 +13,307 @@ using namespace std;
 
 // Support functions ----------------------------------------------------------
 
-extern "C"
+static void
+factorize (Matrix<double> & A, Vector<int> & pivots)
 {
-  void dsytf2_ (const char & uplo,
-				const int & N,
-				double A[],
-				const int & LDA,
-				int IPIV[],
-				int & INFO);
+  // Factorize A as U*D*U' using the upper triangle of A
 
-  void dsytrs_ (const char & uplo,
-				const int & N,
-				const int & NRHS,
-				const double A[],
-				const int & LDA,
-				const int IPIV[],
-				double B[],
-				const int & LDB,
-				int & INFO);
+  const double alpha = (1.0 + sqrt (17.0)) / 8.0;
+  int n = A.columns ();
+
+  pivots.resize (n);
+
+  // K is the main loop index, decreasing from N to 1 in steps of
+  // 1 or 2
+  int k = n - 1;
+  while (k >= 0)
+  {
+	// Determine rows and columns to be interchanged and whether
+	// a 1-by-1 or 2-by-2 pivot block will be used
+
+	int kstep = 1;
+
+	double absakk = fabs (A(k,k));
+
+	// IMAX is the row-index of the largest off-diagonal element in
+	// column K, and COLMAX is its absolute value
+	int imax = 0;
+	double colmax = 0;
+	for (int i = 0; i < k; i++)
+	{
+	  double value = fabs (A(i,k));
+	  if (value > colmax)
+	  {
+		imax = i;
+		colmax = value;
+	  }
+	}
+
+	int kp;
+	if (max (absakk, colmax) == 0)
+	{
+	  throw -k;
+	  //kp = k;
+	}
+	else
+	{
+	  if (absakk >= alpha * colmax)
+	  {
+		// no interchange, use 1-by-1 pivot block
+		kp = k;
+	  }
+	  else
+	  {
+		// JMAX is the column-index of the largest off-diagonal
+		// element in row IMAX, and ROWMAX is its absolute value
+		// (jmax isn't used in this C++ adaptation)
+		double rowmax = 0;
+		for (int j = imax + 1; j <= k; j++)
+		{
+		  rowmax = max (rowmax, fabs (A(imax,j)));
+		}
+		for (int j = 0; j < imax; j++)
+		{
+		  rowmax = max (rowmax, fabs (A(j,imax)));
+		}
+
+		if (absakk >= alpha * colmax * colmax / rowmax)
+		{
+		  // no interchange, use 1-by-1 pivot block
+		  kp = k;
+		}
+		else if (fabs (A(imax,imax)) >= alpha * rowmax)
+		{
+		  // interchange rows and columns K and IMAX, use 1-by-1 pivot block
+		  kp = imax;
+		}
+		else
+		{
+		  // interchange rows and columns K-1 and IMAX, use 2-by-2 pivot block
+		  kp = imax;
+		  kstep = 2;
+		}
+	  }
+
+	  int kk = k - kstep + 1;
+	  if (kp != kk)  // then kp < kk
+	  {
+		// Interchange rows and columns KK and KP in the leading
+		// submatrix A(1:k,1:k)
+		for (int j = 0; j < kp; j++)
+		{
+		  swap (A(j,kk), A(j,kp));
+		}
+		for (int j = kp + 1; j < kk; j++)
+		{
+		  swap (A(j,kk), A(kp,j));
+		}
+		swap (A(kk,kk), A(kp,kp));
+		if (kstep == 2)
+		{
+		  swap (A(k-1,k), A(kp,k));
+		}
+	  }
+
+	  // Update the leading submatrix
+	  if (kstep == 1)
+	  {
+		// 1-by-1 pivot block D(k): column k now holds W(k) = U(k)*D(k)
+		// where U(k) is the k-th column of U
+
+		// Perform a rank-1 update of A(1:k-1,1:k-1) as
+		// A := A - U(k)*D(k)*U(k)' = A - W(k)*1/D(k)*W(k)'
+		double dk = A(k,k);
+		for (int j = 0; j < k; j++)
+		{
+		  if (A(j,k) != 0)
+		  {
+			double temp = -A(j,k) / dk;
+			for (int i = 0; i <= j; i++)
+			{
+			  A(i,j) += A(i,k) * temp;
+			}
+		  }
+		}
+
+		// Store U(k) in column k
+		MatrixRegion<double> (A, 0, k, k-1, k) /= dk;
+	  }
+	  else
+	  {
+		// 2-by-2 pivot block D(k): columns k and k-1 now hold
+		// ( W(k-1) W(k) ) = ( U(k-1) U(k) )*D(k)
+		// where U(k) and U(k-1) are the k-th and (k-1)-th columns of U
+
+		// Perform a rank-2 update of A(1:k-2,1:k-2) as
+		// A := A - ( U(k-1) U(k) )*D(k)*( U(k-1) U(k) )'
+		//    = A - ( W(k-1) W(k) )*inv(D(k))*( W(k-1) W(k) )'
+		double d12 = A(k-1,k);
+		double d22 = A(k-1,k-1);
+		double d11 = A(k,k);
+		double temp = d12;
+		d12 = d11 * d22 / d12 - d12;
+		d22 /= temp;
+		d11 /= temp;
+
+		for (int j = k - 2; j >= 0; j--)
+		{
+		  double wkm1 = (d11 * A(j,k-1) - A(j,k))   / d12;
+		  double wk   = (d22 * A(j,k)   - A(j,k-1)) / d12;
+		  for (int i = j; i >= 0; i--)
+		  {
+			A(i,j) -= A(i,k) * wk + A(i,k-1) * wkm1;
+		  }
+		  A(j,k)   = wk;
+		  A(j,k-1) = wkm1;
+		}
+	  }
+	}
+
+	// Store details of the interchanges in IPIV
+	// Pivot values must be one-based so that negation can work.  The output
+	// of this routine is therefore compatible with dsytf2, etc.
+	kp++;
+	if (kstep == 1)
+	{
+	  pivots[k] = kp;
+	}
+	else
+	{
+	  pivots[k] = -kp;
+	  pivots[k-1] = -kp;
+	}
+
+	// Decrease K
+	k -= kstep;
+  }
+}
+
+static void
+solve (const Matrix<double> & A, const Vector<int> & pivots, Vector<double> & x, const Vector<double> & b)
+{
+  // Solve A*X = B, where A = U*D*U'.
+
+  int n = A.columns ();
+
+  x.copyFrom (b);
+
+  // First solve U*D*X = B
+  // K is the main loop index, decreasing from N-1 to 0 in steps of
+  // 1 or 2, depending on the size of the diagonal blocks.
+  int k = n - 1;
+  while (k >= 0)
+  {
+	if (pivots[k] > 0)
+	{
+	  // 1 x 1 diagonal block
+
+	  // Interchange rows K and IPIV(K).
+	  int kp = pivots[k] - 1;
+	  if (kp != k)
+	  {
+		swap (x[k], x[kp]);
+	  }
+
+	  // Multiply by inv(U(K)), where U(K) is the transformation
+	  // stored in column K of A.
+	  for (int i = 0; i < k; i++)
+	  {
+		x[i] -= A(i,k) * x[k];
+	  }
+
+	  // Multiply by the inverse of the diagonal block.
+	  x[k] /= A(k,k);
+
+	  k--;
+	}
+	else
+	{
+	  // 2 x 2 diagonal block
+
+	  // Interchange rows K-1 and -IPIV(K).
+	  int kp = - pivots[k] - 1;
+	  if (kp != k - 1)
+	  {
+		swap (x[k-1], x[kp]);
+	  }
+
+	  // Multiply by inv(U(K)), where U(K) is the transformation
+	  // stored in columns K-1 and K of A.
+	  for (int i = 0; i < k - 1; i++)
+	  {
+		x[i] -= A(i,k) * x[k];
+	  }
+	  for (int i = 0; i < k - 1; i++)
+	  {
+		x[i] -= A(i,k-1) * x[k-1];
+	  }
+
+	  // Multiply by the inverse of the diagonal block.
+	  double akm1k = A(k-1,k);
+	  double akm1  = A(k-1,k-1) / akm1k;
+	  double ak    = A(k,k)     / akm1k;
+	  double denom = akm1 * ak - 1;
+	  double bkm1  = x[k-1] / akm1k;
+	  double bk    = x[k]   / akm1k;
+	  x[k-1] = (ak   * bkm1 - bk)   / denom;
+	  x[k]   = (akm1 * bk   - bkm1) / denom;
+
+	  k -= 2;
+	}
+  }
+
+  // Next solve U'*X = B
+  // K is the main loop index, increasing from 0 to N-1 in steps of
+  // 1 or 2, depending on the size of the diagonal blocks.
+  k = 0;
+  while (k < n)
+  {
+	if (pivots[k] > 0)
+	{
+	  // 1 x 1 diagonal block
+
+	  // Multiply by inv(U'(K)), where U(K) is the transformation
+	  // stored in column K of A.
+	  for (int i = 0; i < k; i++)
+	  {
+		x[k] -= A(i,k) * x[i];
+	  }
+
+	  // Interchange rows K and IPIV(K)
+	  int kp = pivots[k] - 1;
+	  if (kp != k)
+	  {
+		swap (x[k], x[kp]);
+	  }
+
+	  k++;
+	}
+	else
+	{
+	  // 2 x 2 diagonal block
+
+	  // Multiply by inv(U'(K+1)), where U(K+1) is the transformation
+	  // stored in columns K and K+1 of A.
+	  for (int i = 0; i < k; i++)
+	  {
+		x[k] -= A(i,k) * x[i];
+	  }
+	  for (int i = 0; i < k; i++)
+	  {
+		x[k+1] -= A(i,k+1) * x[i];
+	  }
+
+	  // Interchange rows K and -IPIV(K).
+	  int kp = - pivots[k] - 1;
+	  if (kp != k)
+	  {
+		swap (x[k], x[kp]);
+	  }
+
+	  k += 2;
+	}
+  }
 }
 
 static void
@@ -51,11 +327,14 @@ lmpar (const Matrix<double> & fjac, const Vector<double> & diag, const Vector<do
   Matrix<double> JJ = ~fjac * fjac;
   Matrix<double> factoredJJ;
   factoredJJ.copyFrom (JJ);
-  Vector<int> ipvt (n);
-  int info;
-  dsytf2_ ('U', n, &factoredJJ(0,0), n, &ipvt[0], info);
-  x.copyFrom (Jf);
-  dsytrs_ ('U', n, 1, &factoredJJ(0,0), n, &ipvt[0], &x[0], n, info);
+  Vector<int> ipvt;
+  factorize (factoredJJ, ipvt);
+  solve (factoredJJ, ipvt, x, Jf);
+//cerr << "x1=" << x.frob (1) << " " << fjac.frob (1) << " " << Jf.frob (1) << " " << JJ.frob (1) << " " << factoredJJ.frob (1) << endl;
+//cerr << "JJ=" << JJ << endl;
+//cerr << "Jf=" << Jf << endl;
+//cerr << "x=" << x << endl;
+//cerr << endl;
 
   // Evaluate the function at the origin, and test
   // for acceptance of the gauss-newton direction.
@@ -66,6 +345,11 @@ lmpar (const Matrix<double> & fjac, const Vector<double> & diag, const Vector<do
   }
   double dxnorm = dx.frob (2);
   double fp = dxnorm - delta;
+//cerr << "fp=" << fp << " " << dxnorm << " " << delta << endl;
+//cerr << "x=" << x << endl;
+//cerr << "d=" << diag << endl;
+//cerr << "dx=" << dx << endl;
+//cerr << "dxnorm=" << dxnorm << endl;
   if (fp <= 0.1 * delta)
   {
 	par = 0;
@@ -81,8 +365,7 @@ lmpar (const Matrix<double> & fjac, const Vector<double> & diag, const Vector<do
 	wa1[j] = diag[j] * dx[j] / dxnorm;
   }
   Vector<double> wa2;
-  wa2.copyFrom (wa1);
-  dsytrs_ ('U', n, 1, &factoredJJ(0,0), n, &ipvt[0], &wa2[0], n, info);
+  solve (factoredJJ, ipvt, wa2, wa1);
   double parl = fp / (delta * wa1.dot (wa2));
 
   // Calculate an upper bound, paru, for the zero of the function.
@@ -121,9 +404,11 @@ lmpar (const Matrix<double> & fjac, const Vector<double> & diag, const Vector<do
 	{
 	  factoredJJ(j,j) += par * diag[j] * diag[j];
 	}
-	dsytf2_ ('U', n, &factoredJJ(0,0), n, &ipvt[0], info);
-	x.copyFrom (Jf);
-	dsytrs_ ('U', n, 1, &factoredJJ(0,0), n, &ipvt[0], &x[0], n, info);
+//cerr << "JJ=" << factoredJJ << endl;
+	factorize (factoredJJ, ipvt);
+	solve (factoredJJ, ipvt, x, Jf);
+//cerr << "x=" << x << endl;
+//cerr << endl;
 
 	for (int j = 0; j < n; j++)
 	{
@@ -133,6 +418,7 @@ lmpar (const Matrix<double> & fjac, const Vector<double> & diag, const Vector<do
 	double oldFp = fp;
 	fp = dxnorm - delta;
 
+//cerr << "par=" << par << " " << parl << " " << paru << " " << fp << " " << delta << endl;
 	// If the function is small enough, accept the current value
 	// of par.  Also test for the exceptional cases where parl
 	// is zero or the number of iterations has reached 10.
@@ -148,9 +434,7 @@ lmpar (const Matrix<double> & fjac, const Vector<double> & diag, const Vector<do
 	{
 	  wa1[j] = diag[j] * dx[j] / dxnorm;
 	}
-	Vector<double> wa2;
-	wa2.copyFrom (wa1);
-	dsytrs_ ('U', n, 1, &factoredJJ(0,0), n, &ipvt[0], &wa2[0], n, info);
+	solve (factoredJJ, ipvt, wa2, wa1);
 	double parc = fp / (delta * wa1.dot (wa2));
 
 	// Depending on the sign of the function, update parl or paru.
@@ -168,9 +452,9 @@ lmpar (const Matrix<double> & fjac, const Vector<double> & diag, const Vector<do
 }
 
 
-// class LevenbergMarquardtSparse ---------------------------------------------
+// class LevenbergMarquardtBK -------------------------------------------------
 
-LevenbergMarquardtSparse::LevenbergMarquardtSparse (double toleranceF, double toleranceX, int maxIterations)
+LevenbergMarquardtBK::LevenbergMarquardtBK (double toleranceF, double toleranceX, int maxIterations)
 {
   this->maxIterations = maxIterations;
 
@@ -188,7 +472,7 @@ LevenbergMarquardtSparse::LevenbergMarquardtSparse (double toleranceF, double to
 }
 
 void
-LevenbergMarquardtSparse::search (Searchable & searchable, Vector<double> & point)
+LevenbergMarquardtBK::search (Searchable & searchable, Vector<double> & point)
 {
   // The following is a loose paraphrase the MINPACK function lmdif
 
@@ -209,9 +493,11 @@ LevenbergMarquardtSparse::search (Searchable & searchable, Vector<double> & poin
   double delta;
 
   // outer loop
-  int iter = 1;
+  int iter = 0;
   while (true)
   {
+	iter++;
+
 	// calculate the jacobian matrix
 	searchable.jacobian (point, fjac, &fvec);
 
@@ -288,8 +574,7 @@ LevenbergMarquardtSparse::search (Searchable & searchable, Vector<double> & poin
 	  lmpar (fjac, diag, fvec, delta, par, p);
 
 	  // store the direction p and x + p. calculate the norm of p.
-	  p *= -1;
-	  Vector<double> xp = point + p;
+	  Vector<double> xp = point - p;  // p is actually negative
 	  double pnorm = 0;
 	  for (int j = 0; j < n; j++)
 	  {
@@ -311,16 +596,17 @@ LevenbergMarquardtSparse::search (Searchable & searchable, Vector<double> & poin
 
 	  // compute the scaled actual reduction
 	  double actred = -1;
-	  if (0.1 * fnorm1 < fnorm)
+	  if (fnorm1 < fnorm * 10)
 	  {
 		double temp = fnorm1 / fnorm;
 		actred = 1 - temp * temp;
 	  }
+//cerr << "actred=" << actred << " " << fnorm1 << " " << fnorm << endl;
 
 	  // compute the scaled predicted reduction and the scaled directional derivative
 	  double temp1 = (fjac * p).frob (2) / fnorm;
 	  double temp2 = sqrt (par) * pnorm / fnorm;
-	  double prered = temp1 * temp1 + temp2 * temp2 / 0.5;
+	  double prered = temp1 * temp1 + 2 * temp2 * temp2;
 	  double dirder = -(temp1 * temp1 + temp2 * temp2);
 
 	  // compute the ratio of the actual to the predicted reduction
@@ -333,28 +619,28 @@ LevenbergMarquardtSparse::search (Searchable & searchable, Vector<double> & poin
 	  // update the step bound
 	  if (ratio <= 0.25)
 	  {
-		double temp;
+		double update;
 		if (actred >= 0)
 		{
-		  temp = 0.5;
+		  update = 0.5;
 		}
 		else
 		{
-		  temp = 0.5 * dirder / (dirder + 0.5 * actred);
+		  update = dirder / (2 * dirder + actred);
 		}
-		if (0.1 * fnorm1 >= fnorm  ||  temp < 0.1)
+		if (fnorm1 >= fnorm * 10  ||  update < 0.1)
 		{
-		  temp = 0.1;
+		  update = 0.1;
 		}
-		delta = temp * min (delta, pnorm / 0.1);
-		par /= temp;
+		delta = update * min (delta, pnorm * 10);
+		par /= update;
 	  }
 	  else
 	  {
 		if (par == 0  ||  ratio >= 0.75)
 		{
-		  delta = pnorm / 0.5;
-		  par = 0.5 * par;
+		  delta = pnorm * 2;
+		  par /= 2;
 		}
 	  }
 
@@ -368,19 +654,19 @@ LevenbergMarquardtSparse::search (Searchable & searchable, Vector<double> & poin
 		xnorm = 0;
 		for (int j = 0; j < n; j++)
 		{
-		  double temp = diag[j] + point[j];
+		  double temp = diag[j] * point[j];
 		  xnorm += temp * temp;
 		}
 		xnorm = sqrt (xnorm);
 
 		fnorm = fnorm1;
-		iter++;
 	  }
+//cerr << "ratio=" << ratio << endl;
 
 	  // tests for convergence
 	  if (   fabs (actred) <= toleranceF
 		  && prered <= toleranceF
-		  && 0.5 * ratio <= 1)
+		  && ratio <= 2)
 	  {
 		// info = 1;
 		return;
@@ -398,7 +684,7 @@ LevenbergMarquardtSparse::search (Searchable & searchable, Vector<double> & poin
 	  }
 	  if (   fabs (actred) <= DBL_EPSILON
 		  && prered <= DBL_EPSILON
-		  && 0.5 * ratio <= 1)
+		  && ratio <= 2)
 	  {
 		throw (int) 6;
 	  }
