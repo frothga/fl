cc7d967874536e14670b0effe45d204eb8d0c4a1
rothgang <none@none>
2003-07-17 02:07:07 -0600

    Add dynamicly sized nms neighborhood.  Limit points to local maxima in scale space.  Add one quantum of padding at each end of scale range to accurately find local scale maxima.

--- src/image/InterestLaplacian.cc
+++ src/image/InterestLaplacian.cc
@@ -13,22 +13,40 @@ using namespace fl;
 
 // class InterestLaplacian ----------------------------------------------------
 
-InterestLaplacian::InterestLaplacian (int maxPoints, float thresholdFactor, int neighborhood, int firstStep, int lastStep, float stepSize, int extraSteps)
-: nms (neighborhood)
+InterestLaplacian::InterestLaplacian (int maxPoints, float thresholdFactor, float neighborhood, int firstStep, int lastStep, float stepSize, int extraSteps)
 {
   this->maxPoints       = maxPoints;
   this->thresholdFactor = thresholdFactor;
   this->extraSteps      = extraSteps;
+
+  if (neighborhood > 0)
+  {
+	neighborhood = ceilf (neighborhood);
+  }
+  else if (neighborhood == 0)
+  {
+	neighborhood = 1;
+  }
+  this->neighborhood = neighborhood;
 
   if (stepSize < 0)
   {
 	stepSize = sqrtf (2.0);
   }
 
+  if (firstStep > 0)
+  {
+	firstStep--;
+  }
+  lastStep++;
+
   // Factor in extraSteps for scale searching
   stepSize = powf (stepSize, 1.0f / extraSteps);
   firstStep *= extraSteps;
   lastStep *= extraSteps;
+
+  this->stepSize = stepSize;
+  this->firstStep = firstStep;
 
   // Generate Laplacian filters
   for (int s = firstStep; s <= lastStep; s++)
@@ -43,31 +61,21 @@ InterestLaplacian::InterestLaplacian (int maxPoints, float thresholdFactor, int 
 inline void
 InterestLaplacian::findScale (const Image & image, PointInterest & p)
 {
-  p.weight = 0;
-  p.scale = 1.0;
-  vector<Laplacian>::iterator i;
-  for (i = laplacians.begin (); i < laplacians.end (); i++)
+  int s = (int) rint (logf (p.scale) / logf (stepSize)) - firstStep;
+  int l = max (0, s - extraSteps);
+  int h = min ((int) laplacians.size () - 1, s + extraSteps);
+  for (int i = l + 1; i < h; i++)
   {
-	float response = fabsf (i->response (image, p));
+	float response = fabsf (laplacians[i].response (image, p));
 	if (response > p.weight)
 	{
 	  p.weight = response;
-	  p.scale = i->sigma;
+	  p.scale = laplacians[i].sigma;
 	}
   }
-
-  /*  Technically, a scale at either end of our range is not a local extreme,
-	  because we haven't measured the value beyond it to see if it drops off
-	  again.  However, suppressing the scale back to 1 doesn't make any more
-	  sense than using the end scale...
-  if (p.scale <= laplacians.front ().sigma  ||  p.scale == laplacians.back ().sigma)
-  {
-	p.scale = 1.0;
-	p.weight = 0;
-  }
-  */
 }
 
+/*
 void
 InterestLaplacian::run (const Image & image, multiset<PointInterest> & result)
 {
@@ -88,8 +96,20 @@ startTime = getTimestamp ();
     int offset = laplacians[i].width / 2;
 
 	ImageOf<float> filtered = work * laplacians[i] * abs;
-	filtered *= nms;
 cerr << "  took " << getTimestamp () - startTime << endl;
+startTime = getTimestamp ();
+
+	int nmsSize;
+	if (neighborhood < 0)
+	{
+	  nmsSize = (int) ceilf (-neighborhood * laplacians[i].sigma);
+	}
+	else
+	{
+	  nmsSize = (int) neighborhood;
+	}
+	filtered *= NonMaxSuppress (nmsSize);
+cerr << "  nms " << nmsSize << " " << getTimestamp () - startTime << endl;
 startTime = getTimestamp ();
 
     for (int y = 0; y < filtered.height; y++)
@@ -108,10 +128,18 @@ startTime = getTimestamp ();
 cerr << "  max " << getTimestamp () - startTime << endl;
   }
 
-startTime = getTimestamp ();
-  maxImage *= nms;
-cerr << "nms " << getTimestamp () - startTime << endl;
-  float threshold = nms.average * thresholdFactor;
+  //maxImage *= nms;
+  float average = 0;
+  for (int y = 0; y < maxImage.height; y++)
+  {
+	for (int x = 0; x < maxImage.width; x++)
+	{
+	  average += maxImage (x, y);
+	}
+  }
+  average /= maxImage.width * maxImage.height;
+  float threshold = average * thresholdFactor;
+cerr << "threshold = " << threshold << " = " << average << " * " << thresholdFactor << endl;
 
   result.clear ();
 
@@ -140,10 +168,85 @@ startTime = getTimestamp ();
 cerr << "thresholding " << getTimestamp () - startTime << endl;
 
 startTime = getTimestamp ();
+cerr << "refine scales ";
   multiset<PointInterest>::iterator it;
   for (it = result.begin (); it != result.end (); it++)
   {
 	findScale (work, const_cast<PointInterest &> (*it));
   }
-cerr << "find scale " << getTimestamp () - startTime << endl;
+cerr << getTimestamp () - startTime << endl;
 }
+*/
+
+
+void
+InterestLaplacian::run (const Image & image, std::multiset<PointInterest> & result)
+{
+  ImageOf<float> work = image * GrayFloat;
+  result.clear ();
+
+  AbsoluteValue abs;
+
+  for (int i = extraSteps; i < laplacians.size () - extraSteps; i += extraSteps)
+  {
+cerr << "filter " << i;
+double startTime = getTimestamp ();
+
+    int offset = laplacians[i].width / 2;
+
+	ImageOf<float> filtered = work * laplacians[i] * abs;
+
+	int nmsSize;
+	if (neighborhood < 0)
+	{
+	  nmsSize = (int) ceilf (-neighborhood * laplacians[i].sigma);
+	}
+	else
+	{
+	  nmsSize = (int) neighborhood;
+	}
+	NonMaxSuppress nms (nmsSize);
+	filtered *= nms;
+	float threshold = nms.average * thresholdFactor;
+cerr << "threshold = " << threshold << " = " << nms.average << " * " << thresholdFactor << endl;
+
+	for (int y = 0; y < filtered.height; y++)
+	{
+	  for (int x = 0; x < filtered.width; x++)
+	  {
+		float pixel = filtered (x, y);
+		if (pixel > threshold)
+		{
+		  PointInterest p;
+		  p.x = x + offset;
+		  p.y = y + offset;
+
+		  float r0 = fabsf (laplacians[i - extraSteps].response (work, p));
+		  float r2 = fabsf (laplacians[i + extraSteps].response (work, p));
+		  if (pixel > r0  &&  pixel > r2  &&  r0 > 0  &&  r2 > 0)
+		  {
+			p.scale = laplacians[i].sigma;
+			p.weight = pixel;
+			p.detector = PointInterest::Laplacian;
+			result.insert (p);
+			if (result.size () > maxPoints)
+			{
+			  result.erase (result.begin ());
+			}
+		  }
+		}
+	  }
+	}
+cerr << " " << getTimestamp () - startTime << endl;
+  }
+
+double startTime = getTimestamp ();
+cerr << "refine scales ";
+  multiset<PointInterest>::iterator it;
+  for (it = result.begin (); it != result.end (); it++)
+  {
+	findScale (work, const_cast<PointInterest &> (*it));
+  }
+cerr << getTimestamp () - startTime << endl;
+}
+
