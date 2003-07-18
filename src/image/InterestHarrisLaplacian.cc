612adc8063b45843198addcf05aef5487061750f
rothgang <none@none>
2003-07-18 13:08:08 -0600

    Change method for generating scales.  Add "bookending", where the set of scales is one quantum larger at each end than the respective range of Harris filters.  Add code to properly detect whether the point is a local maximum in scale space.  (Unfortunately, tests on a rotated image show that any type of scale maximum filtering reduces the repeatability of points found by this detector.)

--- src/image/InterestHarrisLaplacian.cc
+++ src/image/InterestHarrisLaplacian.cc
@@ -12,138 +12,75 @@ using namespace fl;
 
 // class InterestHarrisLaplacian ----------------------------------------------
 
-InterestHarrisLaplacian::InterestHarrisLaplacian (int maxPoints, float thresholdFactor, int neighborhood, int firstStep, int lastStep, float stepSize)
-: nms (neighborhood)
+InterestHarrisLaplacian::InterestHarrisLaplacian (int maxPoints, float thresholdFactor, float neighborhood, float firstScale, float lastScale, int extraSteps, float stepSize)
 {
   this->maxPoints       = maxPoints;
   this->thresholdFactor = thresholdFactor;
+  this->extraSteps      = extraSteps;
+
+  if (neighborhood > 0)
+  {
+	neighborhood = ceilf (neighborhood);
+  }
+  else if (neighborhood == 0)
+  {
+	neighborhood = 1;
+  }
+  this->neighborhood = neighborhood;
 
   if (stepSize < 0)
   {
 	stepSize = sqrtf (2.0);
   }
-  halfStep = sqrtf (stepSize);
+  stepSize = powf (stepSize, 1.0f / extraSteps);
+  this->stepSize = stepSize;
 
+  firstStep = max (0, (int) rint (logf (firstScale) / logf (stepSize)) - extraSteps);
+  int lastStep = (int) ceil ((logf (lastScale) / logf (stepSize) - firstStep) / extraSteps) * extraSteps + firstStep;
+
+  // Generate Laplacian filters
   for (int s = firstStep; s <= lastStep; s++)
   {
 	float scale = powf (stepSize, s);
-	FilterHarrisEigen h (scale * 0.7125, scale);  // sigmaI seems to be the truer representative of characteristic scale
-	filters.push_back (h);
-  }
-
-  float maxScale = powf (stepSize, lastStep + 0.5);
-  stepSize = powf (stepSize, 1.0 / 3.0);
-  lastStep = (int) (logf (maxScale) / logf (stepSize));
-cerr << "lastStep = " << lastStep << endl;
-cerr << "maxScale = " << maxScale << endl;
-cerr << "creating scales: ";
-  for (int s = 0; s <= lastStep; s++)
-  {
-	float scale = powf (stepSize, s);
-cerr << scale << " ";
 	Laplacian l (scale);
 	l *= scale * scale;
 	laplacians.push_back (l);
   }
-cerr << endl;
+
+  // Generate Harris filters
+  for (int s = firstStep + extraSteps; s <= lastStep - extraSteps; s += extraSteps)
+  {
+	float scale = powf (stepSize, s);
+	cerr << "harris scale = " << scale << endl;
+	FilterHarris h (scale * 0.7125, scale);  // sigmaI seems to be the truer representative of characteristic scale
+	filters.push_back (h);
+  }
 }
 
 inline void
 InterestHarrisLaplacian::findScale (const Image & image, PointInterest & p)
 {
+  float harrisWeight = p.weight;
   p.weight = 0;
-  p.scale = 1.0;
-  vector<Laplacian>::iterator i;
-  for (i = laplacians.begin (); i < laplacians.end (); i++)
+
+  int s = (int) rint (logf (p.scale) / logf (stepSize)) - firstStep;
+  int l = max (0, s - extraSteps);
+  int h = min ((int) laplacians.size () - 1, s + extraSteps);
+  for (int i = l + 1; i < h; i++)
   {
-	float response = fabs (i->response (image, p));
+	float response = fabsf (laplacians[i].response (image, p));
 	if (response > p.weight)
 	{
 	  p.weight = response;
-	  p.scale = i->sigma;
+	  p.scale = laplacians[i].sigma;
 	}
   }
 
-  if (p.scale <= laplacians.front ().sigma  ||  p.scale >= laplacians.back ().sigma)
-  {
-	p.scale = 1.0;
-	p.weight = 0;
-  }
+  // p.weight could either be max Laplacian response or max Harris response
+  // but probably Harris response is more meaningful since this is a Harris
+  // detector.  :)
+  p.weight = harrisWeight;
 }
-
-/*
-void
-InterestHarrisLaplacian::run (const Image & image, std::multiset<PointInterest> & result)
-{
-  ImageOf<float> work = image * GrayFloat;
-
-  ImageOf<float> maxImage (work.width, work.height, GrayFloat);
-  maxImage.clear ();
-double startTime;
-  for (int i = 0; i < filters.size (); i++)
-  {
-cerr << "filter " << i << endl;
-startTime = getTimestamp ();
-    //int offset = max (filters[i].G1.width, filters[i].dG.width) / 2;
-    //int offsetX = filters[i].G.width / 2 + offset;
-    //int offsetY = filters[i].G.height / 2 + offset;
-    int offset = filters[i].offset;
-
-	ImageOf<float> filtered = work * filters[i];
-	filtered *= nms;
-cerr << "  took " << getTimestamp () - startTime << endl;
-startTime = getTimestamp ();
-
-	for (int x = 0; x < filtered.width; x++)
-	{
-	  for (int y = 0; y < filtered.height; y++)
-	  {
-		//float & target = maxImage.pixelGrayFloat (x + offsetX, y + offsetY);
-		float & target = maxImage (x + offset, y + offset);
-		float & source = filtered (x, y);
-		target = max (target, source);
-	  }
-	}
-cerr << "  max " << getTimestamp () - startTime << endl;
-  }
-
-startTime = getTimestamp ();
-  maxImage *= nms;
-cerr << "nms " << getTimestamp () - startTime << endl;
-  float threshold = nms.average * thresholdFactor;
-
-  result.clear ();
-
-startTime = getTimestamp ();
-  for (int x = 0; x < maxImage.width; x++)
-  {
-	for (int y = 0; y < maxImage.height; y++)
-	{
-	  float pixel = maxImage (x, y);
-	  if (pixel > threshold)
-	  {
-		PointInterest p;
-		p.weight = pixel;
-		p.x = x;
-		p.y = y;
-		result.insert (p);
-		if (result.size () > maxPoints)
-		{
-		  result.erase (result.begin ());
-		}
-	  }
-	}
-  }
-cerr << "thresholding " << getTimestamp () - startTime << endl;
-
-  // Calculate natural scale for each selected interest point using Laplacian
-  multiset<PointInterest>::iterator p;
-  for (p = result.begin (); p != result.end (); p++)
-  {
-	findScale (image, (PointInterest &) *p);
-  }
-}
-*/
 
 void
 InterestHarrisLaplacian::run (const Image & image, std::multiset<PointInterest> & result)
@@ -159,12 +96,23 @@ double startTime = getTimestamp ();
     int offset = filters[i].offset;
 
 	ImageOf<float> filtered = work * filters[i];
+
+	int nmsSize;
+	if (neighborhood < 0)
+	{
+	  nmsSize = (int) ceilf (-neighborhood * filters[i].sigmaI);
+	}
+	else
+	{
+	  nmsSize = (int) neighborhood;
+	}
+	NonMaxSuppress nms (nmsSize);
 	filtered *= nms;
 	float threshold = nms.average * thresholdFactor;
 
-	for (int x = 0; x < filtered.width; x++)
+	for (int y = 0; y < filtered.height; y++)
 	{
-	  for (int y = 0; y < filtered.height; y++)
+	  for (int x = 0; x < filtered.width; x++)
 	  {
 		float pixel = filtered (x, y);
 		if (pixel > threshold)
@@ -172,11 +120,13 @@ double startTime = getTimestamp ();
 		  PointInterest p;
 		  p.x = x + offset;
 		  p.y = y + offset;
-		  findScale (work, p);
-		  float scaleRatio = p.scale / filters[i].sigmaI;
-		  if (scaleRatio < halfStep  &&  scaleRatio > 1.0 / halfStep)
+
+		  float r0 = fabsf (laplacians[ i      * extraSteps].response (work, p));  // one step down
+		  float r1 = fabsf (laplacians[(i + 1) * extraSteps].response (work, p));  // Current scale
+		  float r2 = fabsf (laplacians[(i + 2) * extraSteps].response (work, p));  // one step up
+		  if (r1 > r0  &&  r1 > r2)
 		  {
-			// p.weight could either be max Laplacian response or max Harris response
+			p.scale = filters[i].sigmaI;
 			p.weight = pixel;
 			p.detector = PointInterest::Harris;
 			result.insert (p);
@@ -188,7 +138,15 @@ double startTime = getTimestamp ();
 		}
 	  }
 	}
-
 cerr << " " << getTimestamp () - startTime << endl;
   }
+
+double startTime = getTimestamp ();
+cerr << "refine scales ";
+  multiset<PointInterest>::iterator it;
+  for (it = result.begin (); it != result.end (); it++)
+  {
+	findScale (work, const_cast<PointInterest &> (*it));
+  }
+cerr << getTimestamp () - startTime << endl;
 }
