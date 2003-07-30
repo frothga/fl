258bf0765474ded39dfc9f503221afefd3d141b6
rothgang <none@none>
2003-07-30 14:06:45 -0600

    Finish rewriting FilterHarris into FilterHessian.

--- src/image/FilterHessian.cc
+++ src/image/FilterHessian.cc
@@ -6,31 +6,21 @@ using namespace std;
 using namespace fl;
 
 
-// class FilterHarris ---------------------------------------------------------
+// class FilterHessian --------------------------------------------------------
 
-const double FilterHarris::alpha = 0.06;
-
-FilterHarris::FilterHarris (double sigmaD, double sigmaI, const PixelFormat & format)
-: G_I (sigmaI, format),
-  G1_I (sigmaI, format),
-  G1_D (sigmaD, format),
-  dG_D (sigmaD, format),
-  xx (format),
-  xy (format),
-  yy (format)
+FilterHessian::FilterHessian (double sigma, const PixelFormat & format)
+: G (sigma, format),
+  dG (sigma, format)
 {
-  this->sigmaD = sigmaD;
-  this->sigmaI = sigmaI;
+  this->sigma = sigma;
 
-  dG_D *= sigmaD;  // Boost to make results comparable across scale.
+  dG *= sigma * sigma;  // Boost to make results comparable across scale.
 
-  offsetI = G1_I.width / 2;
-  offsetD = (G1_D.width >? dG_D.width) / 2;
-  offset = offsetI + offsetD;
+  offset = max (G.width, dG.width) / 2;
 
   offset1 = 0;
   offset2 = 0;
-  int difference = (G1_D.width - dG_D.width) / 2;
+  int difference = (G.width - dG.width) / 2;
   if (difference > 0)
   {
 	offset1 = difference;
@@ -42,138 +32,51 @@ FilterHarris::FilterHarris (double sigmaD, double sigmaI, const PixelFormat & fo
 }
 
 Image
-FilterHarris::filter (const Image & image)
+FilterHessian::filter (const Image & image)
 {
-  preprocess (image);
-  return process ();
-}
-
-void
-FilterHarris::preprocess (const Image & image)
-{
-  if (*image.format != *G1_D.format)
+  if (*image.format != *G.format)
   {
-	preprocess (image * (*G1_D.format));
+	return filter (image * (*G.format));
   }
 
-  G1_D.direction = Vertical;
-  dG_D.direction = Horizontal;
-  Image dx = image * G1_D * dG_D;
+  G.direction = Vertical;
+  dG.direction = Horizontal;
+  Image dxx = image * G * dG;
 
-  G1_D.direction = Horizontal;
-  dG_D.direction = Vertical;
-  Image dy = image * G1_D * dG_D;
+  G.direction = Horizontal;
+  dG.direction = Vertical;
+  Image dyy = image * G * dG;
 
-  xx.resize (dx.width <? dy.width, dx.height <? dy.height);
-  xy.resize (xx.width, xx.height);
-  yy.resize (xx.width, xx.height);
-
-  if (*dx.format == GrayFloat)
+  if (*dxx.format == GrayFloat)
   {
-	ImageOf<float> dxf (dx);
-	ImageOf<float> dyf (dy);
-	ImageOf<float> xxf (xx);
-	ImageOf<float> xyf (xy);
-	ImageOf<float> yyf (yy);
-	for (int x = 0; x < xx.width; x++)
+	ImageOf<float> result (min (dxx.width, dyy.width), min (dxx.height, dyy.height), GrayFloat);
+	ImageOf<float> dxxf (dxx);
+	ImageOf<float> dyyf (dyy);
+	for (int y = 0; y < result.height; y++)
 	{
-	  for (int y = 0; y < xx.height; y++)
+	  for (int x = 0; x < result.width; x++)
 	  {
-		float tx = dxf (x + offset1, y + offset2);
-		float ty = dyf (x + offset2, y + offset1);
-		xxf (x, y) = tx * tx;
-		xyf (x, y) = tx * ty;
-		yyf (x, y) = ty * ty;
-	  }
-	}
-  }
-  else if (*dx.format == GrayDouble)
-  {
-	ImageOf<double> dxd (dx);
-	ImageOf<double> dyd (dy);
-	ImageOf<double> xxd (xx);
-	ImageOf<double> xyd (xy);
-	ImageOf<double> yyd (yy);
-	for (int x = 0; x < xx.width; x++)
-	{
-	  for (int y = 0; y < xx.height; y++)
-	  {
-		double tx = dxd (x + offset1, y + offset2);
-		double ty = dyd (x + offset2, y + offset1);
-		xxd (x, y) = tx * tx;
-		xyd (x, y) = tx * ty;
-		yyd (x, y) = ty * ty;
-	  }
-	}
-  }
-  else
-  {
-	throw "FilterHarris::preprocess: unimplemented format";
-  }
-}
-
-Image
-FilterHarris::process ()
-{
-  int last = G1_I.width - 1;
-
-  G1_I.direction = Vertical;
-  Image Sxx = xx * G1_I;
-  Image Sxy = xy * G1_I;
-  Image Syy = yy * G1_I;
-  G1_I.direction = Horizontal;
-  Sxx = Sxx * G1_I;
-  Sxy = Sxy * G1_I;
-  Syy = Syy * G1_I;
-
-  if (*G1_I.format == GrayFloat)
-  {
-	ImageOf<float> result (xx.width - last, xx.height - last, GrayFloat);
-	ImageOf<float> Sxxf (Sxx);
-	ImageOf<float> Sxyf (Sxy);
-	ImageOf<float> Syyf (Syy);
-	for (int x = 0; x < result.width; x++)
-	{
-	  for (int y = 0; y < result.height; y++)
-	  {
-		float txx = Sxxf (x, y);
-		float txy = Sxyf (x, y);
-		float tyy = Syyf (x, y);
-		result (x, y) = (txx * tyy - txy * txy) - alpha * (txx + tyy) * (txx + tyy);
+		result (x, y) = dxxf (x + offset1, y + offset2) + dyyf (x + offset2, y + offset1);
 	  }
 	}
 	return result;
   }
-  else if (*G1_I.format == GrayDouble)
+  else if (*dxx.format == GrayDouble)
   {
-	ImageOf<double> result (xx.width - last, xx.height - last, GrayDouble);
-	ImageOf<double> Sxxd (Sxx);
-	ImageOf<double> Sxyd (Sxy);
-	ImageOf<double> Syyd (Syy);
-	for (int x = 0; x < result.width; x++)
+	ImageOf<double> result (min (dxx.width, dyy.width), min (dxx.height, dyy.height), GrayFloat);
+	ImageOf<double> dxxd (dxx);
+	ImageOf<double> dyyd (dyy);
+	for (int y = 0; y < result.height; y++)
 	{
-	  for (int y = 0; y < result.height; y++)
+	  for (int x = 0; x < result.width; x++)
 	  {
-		double txx = Sxxd (x, y);
-		double txy = Sxyd (x, y);
-		double tyy = Syyd (x, y);
-		result (x, y) = (txx * tyy - txy * txy) - alpha * (txx + tyy) * (txx + tyy);
+		result (x, y) = dxxd (x + offset1, y + offset2) + dyyd (x + offset2, y + offset1);
 	  }
 	}
 	return result;
   }
   else
   {
-	throw "FilterHarris::process: unimplemented format";
+	throw "FilterHessian::filter: unimplemented format";
   }
-}
-
-double
-FilterHarris::response (const int x, const int y) const
-{
-  Point p (x + offsetI, y + offsetI);
-  double txx = G_I.response (xx, p);
-  double txy = G_I.response (xy, p);
-  double tyy = G_I.response (yy, p);
-  return (txx * tyy - txy * txy) - alpha * (txx + tyy) * (txx + tyy);
 }
