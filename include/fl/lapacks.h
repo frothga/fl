639dc644410c1f99cea2646e6332deb557f3fdb9
rothgang <none@none>
2003-08-09 21:53:04 -0600

    Bring up to date with lapackd.h

--- include/fl/lapacks.h
+++ include/fl/lapacks.h
@@ -4,9 +4,9 @@
 
 #include "fl/matrix.h"
 #include "fl/lapackprotos.h"
+#include "fl/complex.h"
 
 #include <algorithm>
-#include <complex>
 #include <float.h>
 
 
@@ -14,7 +14,7 @@ namespace fl
 {
   // Compute eigenvalues and eigenvectors for symmetric matrix
   inline void
-  syev (const Matrix<float> & A, Matrix<float> & eigenvalues, Matrix<float> & eigenvectors)
+  syev (const MatrixAbstract<float> & A, Matrix<float> & eigenvalues, Matrix<float> & eigenvectors)
   {
 	int n = A.rows ();
 	eigenvectors.copyFrom (A);
@@ -46,9 +46,41 @@ namespace fl
 	}
   }
 
+  inline void
+  syev (const MatrixPacked<float> & A, Matrix<float> & eigenvalues, Matrix<float> & eigenvectors)
+  {
+	int n = A.rows ();
+
+	MatrixPacked<float> tempA;
+	tempA.copyFrom (A);
+
+	eigenvalues.resize (n);
+	eigenvectors.resize (n, n);
+
+	float * work = new float [3 * n];
+	int info = 0;
+
+	sspev_ ('V',
+			'U',
+			n,
+			& tempA(0,0),
+			& eigenvalues[0],
+			& eigenvectors(0,0),
+			n,
+			work,
+			info);
+
+	delete work;
+
+	if (info)
+	{
+	  throw info;
+	}
+  }
+
   // Compute eigenvalues (only) for symmetric matrix.
   inline void
-  syev (const Matrix<float> & A, Matrix<float> & eigenvalues)
+  syev (const MatrixAbstract<float> & A, Matrix<float> & eigenvalues)
   {
 	int n = A.rows ();
 
@@ -83,9 +115,8 @@ namespace fl
   }
 
   // Compute eigenvalues and eigenvectors for general (non-symmetric) matrix
-  // eigenvalues should really be Matrix< complex<float> >.  Fix this some time.
   inline void
-  geev (const Matrix<float> & A, Matrix<float> & eigenvalues, Matrix<float> & eigenvectors)
+  geev (const MatrixAbstract<float> & A, Matrix<float> & eigenvalues, Matrix<float> & eigenvectors)
   {
 	char jobvl = 'N';
 	char jobvr = 'V';
@@ -127,45 +158,38 @@ namespace fl
 	}
   }
 
-  // Solve least squares problem using SVD via QR
   inline void
-  gelss (Matrix<float> & A, Matrix<float> & x, const Matrix<float> & b)
+  geev (const MatrixAbstract<float> & A, Matrix<float> & eigenvalues)
   {
-	int m = A.rows ();
-	int n = A.columns ();
-	int ldb = m >? n;
-	int bn = b.columns ();
+	int lda = A.rows ();
+	int n = std::min (lda, A.columns ());
 
 	Matrix<float> tempA;
 	tempA.copyFrom (A);
 
-	x.resize (ldb, bn);
-	x.copyFrom (b);
-	x.rows_ = ldb;  // Size of answer x, not necessarily size of b.
+	eigenvalues.resize (n);
+	Matrix<float> wi (n);
 
-	float rcond = -1;
-	int rank;
-    int ldwork = 5 * (ldb >? bn);
-    float * work = new float[ldwork];
-    float * s = new float[m <? n];
+	int lwork = 5 * n;
+	float * work = new float [lwork];
 	int info = 0;
 
-    sgelss_ (m,
-			 n,
-			 bn,
-			 & tempA[0],
-			 m,
-			 & x[0],
-			 ldb,
-			 s,
-			 rcond,
-			 rank,
-			 work,
-			 ldwork,
-			 info);
+	sgeev_ ('N',
+			'N',
+			n,
+			& tempA[0],
+			lda,
+			& eigenvalues[0],
+			& wi[0],
+			0,
+			1,  // ldvl >= 1.  A lie to make lapack happy.
+			0,
+			1,  // ldvr >= 1.  ditto
+			work,
+			lwork,
+			info);
 
-    delete work;
-    delete s;
+	delete work;
 
 	if (info)
 	{
@@ -174,19 +198,157 @@ namespace fl
   }
 
   inline void
-  gesvd (const Matrix<float> & A, Matrix<float> & U, Matrix<float> & S, Matrix<float> & VT)
+  geev (const MatrixAbstract<float> & A, Matrix<complex float> & eigenvalues, Matrix<float> & eigenvectors)
   {
-	char jobu = 'A';
-	char jobvt = 'A';
+	char jobvl = 'N';
+	char jobvr = 'V';
+
+	int lda = A.rows ();
+	int n = lda <? A.columns ();
+	Matrix<float> tempA;
+	tempA.copyFrom (A);
+
+	eigenvalues.resize (n);
+	Matrix<float> wr (n);
+	Matrix<float> wi (n);
+
+	eigenvectors.resize (n, n);
+
+	int lwork = 5 * n;
+	float * work = new float [lwork];
+	int info = 0;
+
+	sgeev_ (jobvl,
+			jobvr,
+			n,
+			& tempA[0],
+			lda,
+			& wr[0],
+			& wi[0],
+			0,
+			1,  // ldvl >= 1.  A lie to make lapack happy.
+			& eigenvectors[0],
+			n,
+			work,
+			lwork,
+			info);
+
+	delete work;
+
+	if (info)
+	{
+	  throw info;
+	}
+
+	for (int i = 0; i < n; i++)
+	{
+	  eigenvalues[i] = wr[i] + wi[i] * I;
+	}
+  }
+
+  // Solve least squares problem using SVD via QR
+  inline int
+  gelss (const MatrixAbstract<float> & A, Matrix<float> & x, const MatrixAbstract<float> & b, const float rcond, Matrix<float> & s)
+  {
 	int m = A.rows ();
 	int n = A.columns ();
+	int ldb = std::max (m, n);
+	int nrhs = b.columns ();
+	int bm = std::max (b.rows (), ldb);
 
 	Matrix<float> tempA;
 	tempA.copyFrom (A);
 
-	U.resize (m, m);
-	S.resize (m <? n);
-	VT.resize (n, n);
+	x.resize (ldb, nrhs);
+	for (int c = 0; c < nrhs; c++)
+	{
+	  for (int r = 0; r < bm; r++)
+	  {
+		x(r,c) = b(r,c);
+	  }
+	}
+
+    s.resize (std::min (m, n), 1);
+
+	int rank;
+    int ldwork = 5 * (ldb >? nrhs);
+    float * work = new float[ldwork];
+	int info = 0;
+
+    sgelss_ (m,
+			 n,
+			 nrhs,
+			 & tempA[0],
+			 m,
+			 & x[0],
+			 ldb,
+			 & s[0],
+			 rcond,
+			 rank,
+			 work,
+			 ldwork,
+			 info);
+
+    delete work;
+
+	if (info)
+	{
+	  throw info;
+	}
+
+	return rank;
+  }
+
+  inline int
+  gelss (const MatrixAbstract<float> & A, Matrix<float> & x, const MatrixAbstract<float> & b, const float rcond = -1)
+  {
+	Matrix<float> s;
+	return gelss (A, x, b, rcond, s);
+  }
+
+  inline void
+  gesvd (const MatrixAbstract<float> & A, Matrix<float> & U, Matrix<float> & S, Matrix<float> & VT, char jobu = 'A', char jobvt = 'A')
+  {
+	int m = A.rows ();
+	int n = A.columns ();
+	int minmn = std::min (m, n);
+
+	Matrix<float> tempA;
+	tempA.copyFrom (A);
+
+	S.resize (minmn);
+
+	switch (jobu)
+	{
+	  case 'A':
+		U.resize (m, m);
+		break;
+	  case 'N':
+		if (U.columns () < 1)
+		{
+		  U.resize (1, 1);
+		}
+		break;
+	  default:
+		jobu = 'S';
+		U.resize (m, minmn);
+	}
+
+	switch (jobvt)
+	{
+	  case 'A':
+		VT.resize (n, n);
+		break;
+	  case 'N':
+		if (VT.columns () < 1)
+		{
+		  VT.resize (1, 1);
+		}
+		break;
+	  default:
+		jobvt = 'S';
+		U.resize (minmn, n);
+	}
 
     int lwork = -1;
 	float optimalSize;
@@ -201,9 +363,9 @@ namespace fl
 			 m,
 			 & S[0],
 			 & U[0],
-			 m,
+			 U.rows (),
 			 & VT[0],
-			 n,
+			 VT.rows (),
 			 &optimalSize,
 			 lwork,
 			 info);
@@ -211,6 +373,7 @@ namespace fl
 	lwork = (int) optimalSize;
     float * work = new float[lwork];
 
+	// Now for the real thing
 	sgesvd_ (jobu,
 			 jobvt,
 			 m,
@@ -219,9 +382,9 @@ namespace fl
 			 m,
 			 & S[0],
 			 & U[0],
-			 m,
+			 U.rows (),
 			 & VT[0],
-			 n,
+			 VT.rows (),
 			 work,
 			 lwork,
 			 info);
