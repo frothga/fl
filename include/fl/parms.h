8b8ca0a58e827a82cd3c8313852c8e7175c8419e
rothgang <none@none>
2003-12-30 16:24:51 -0700

    Add function to output parms in human readable form.  Convert comments to doxygen format.

--- include/fl/parms.h
+++ include/fl/parms.h
@@ -9,9 +9,11 @@
 
 namespace fl
 {
-  // Stores a collection of named values.  Names are caseless, and no name
-  // may be a prefix of another.  Stores values as human readable strings.
-  // Several convenience functions parse the value into different formats.
+  /**
+	 Stores a collection of named values.  Names are caseless, and no name
+	 may be a prefix of another.  Stores values as human readable strings.
+	 Several convenience functions parse the value into different formats.
+  **/
   class Parameters
   {
   public:
@@ -25,20 +27,22 @@ namespace fl
 	void write (const std::string & parmFileName) const;
 	void write (std::ostream & stream) const;
 
-	int find (const std::string & name) const;  // finds index of given name in names vector.  returns -1 if not found.
-	void update (const std::string & name, const std::string & value);  // causes a single entry called "name" to exist and have "value"
+	int find (const std::string & name) const;  ///< finds index of given name in names vector.  returns -1 if not found.
+	void update (const std::string & name, const std::string & value);  ///< causes a single entry called "name" to exist and have "value"
 
 	// For simple types, return the value directly.  For complex structures,
 	// return the value in a reference parameter.
 	char * getChar (const std::string & name, const char * defaultValue = "") const;
 	int getInt (const std::string & name, int defaultValue = 0) const;
 	float getFloat (const std::string & name, float defaultValue = 0) const;
-	void getStringList (const std::string & name, std::vector<std::string> & result, const std::vector<std::string> * defaultValue = NULL) const;  // parses the value into its comma-separated elements
+	void getStringList (const std::string & name, std::vector<std::string> & result, const std::vector<std::string> * defaultValue = NULL) const;  ///< parses the value into its comma-separated elements
 
 	std::vector<std::string> names;
 	std::vector<std::string> values;
-	std::vector<std::string> fileNames;  // All strings that didn't have the form name=value.  Not necessarily file names, but we imagine so.
+	std::vector<std::string> fileNames;  ///< All strings that didn't have the form name=value.  Not necessarily file names, but we imagine so.
   };
+
+  std::ostream & operator << (std::ostream & out, const Parameters & parms);  ///< Dump a human readable summary of the parameters to a stream.
 }
 
 
