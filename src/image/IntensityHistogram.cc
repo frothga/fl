c70d6ae35b4022542827672027ca48c1e56a21b9
rothgang <none@none>
2003-12-30 21:10:10 -0700

    Create filter for measuring the spread of intensities in an image.

---
+++ src/image/IntensityHistogram.cc
@@ -0,0 +1,128 @@
+#include "fl/convolve.h"
+
+
+using namespace std;
+using namespace fl;
+
+
+// class IntensityHistogram ---------------------------------------------------
+
+IntensityHistogram::IntensityHistogram (const vector<float> & ranges)
+{
+  this->ranges = ranges;
+
+  int bins = ranges.size () - 1;
+  counts.resize (bins);
+  for (int i = 0; i < bins; i++)
+  {
+	counts[i] = 0;
+  }
+}
+
+IntensityHistogram::IntensityHistogram (float minimum, float maximum, int bins)
+{
+  float step = (maximum - minimum) / bins;
+
+  ranges.resize (bins + 1);
+  counts.resize (bins);
+
+  for (int i = 0; i < bins; i++)
+  {
+	ranges[i] = minimum + i * step;
+	counts[i] = 0;
+  }
+  ranges[bins] = maximum;
+}
+
+Image
+IntensityHistogram::filter (const Image & image)
+{
+  const int bins = counts.size ();
+  for (int i = 0; i < bins; i++)
+  {
+	counts[i] = 0;
+  }
+
+  #define addup(size) \
+  { \
+	size * pixel = (size *) image.buffer; \
+	size * end   = pixel + image.width * image.height; \
+	while (pixel < end) \
+	{ \
+	  if (*pixel <= ranges.back ()) \
+	  { \
+		for (int i = bins - 1; i >= 0; i--) \
+		{ \
+		  if (*pixel >= ranges[i]) \
+		  { \
+			counts[i]++; \
+			break; \
+		  } \
+		} \
+	  } \
+	  pixel++; \
+	} \
+  }
+
+  if (*image.format == GrayFloat)
+  {
+	addup (float);
+  }
+  else if (*image.format == GrayDouble)
+  {
+	addup (double);
+  }
+  else if (*image.format == GrayChar)
+  {
+	addup (unsigned char);
+  }
+  else
+  {
+	filter (image * GrayFloat);
+  }
+
+  return image;
+}
+
+int
+IntensityHistogram::total () const
+{
+  int result = 0;
+  for (int i = 0; i < counts.size (); i++)
+  {
+	result += counts[i];
+  }
+  return result;
+}
+
+void
+IntensityHistogram::dump (ostream & stream, bool centers, bool percent) const
+{
+  float total;
+  if (percent)
+  {
+	total = this->total ();
+  }
+
+  for (int i = 0; i < counts.size (); i++)
+  {
+	if (centers)
+	{
+	  stream << ((ranges[i] + ranges[i+1]) / 2);
+	}
+	else
+	{
+	  stream << ranges[i];
+	}
+	stream << " ";
+	if (percent)
+	{
+	  stream << (counts[i] / total);
+	}
+	else
+	{
+	  stream << counts[i];
+	}
+	stream << endl;
+  }
+}
