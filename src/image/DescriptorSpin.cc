429c1499ada28c5ee72f65b95969c15c19390c31
rothgang <none@none>
2004-01-23 23:16:20 -0700

    Simplified.

--- src/image/DescriptorSpin.cc
+++ src/image/DescriptorSpin.cc
@@ -3,7 +3,7 @@
 
 // For debugging
 #include "fl/time.h"
-//#include "fl/slideshow.h"
+#include "fl/slideshow.h"
 //#include "fl/canvas.h"
 
 
@@ -12,8 +12,6 @@ using namespace fl;
 
 
 // class DescriptorSpin -------------------------------------------------------
-
-static const float hsqrt2 = sqrt (2.0) / 2.0;  // "half square root of 2".  Used by several InterestSpin... classes
 
 DescriptorSpin::DescriptorSpin (int binsRadial, int binsIntensity, float supportRadial, float supportIntensity)
 {
@@ -44,7 +42,7 @@ DescriptorSpin::value (const Image & image, const PointAffine & point)
   center.scale = half / supportRadial;
   TransformGauss rectify (point.A / (half / (supportRadial * point.scale)), true);
   rectify.setPeg (point.x, point.y, patchSize, patchSize);
-  Image patch = image * rectify;
+  ImageOf<float> patch = image * rectify;
 
   // Determine various sizes
   /*
@@ -67,16 +65,32 @@ DescriptorSpin::value (const Image & image, const PointAffine & point)
   int y2 = patch.width - 1;
 
   // Determine intensity bin values
-  width += hsqrt2;
   float minIntensity;
   float quantum;
   //rangeMeanDeviation (image, point, x1, y1, x2, y2, width, minIntensity, quantum);
   rangeMeanDeviation (patch, center, x1, y1, x2, y2, width, minIntensity, quantum);
 
   // Bin up all the pixels
-  Vector<float> result;
-  //doBinning (image, point, x1, y1, x2, y2, width, minIntensity, quantum, binRadius, result);
-  doBinning (patch, center, x1, y1, x2, y2, width, minIntensity, quantum, binRadius, result);
+  Vector<float> result (binsRadial * binsIntensity);
+  result.clear ();
+  for (int x = x1; x <= x2; x++)
+  {
+	float dx = x - center.x;
+	for (int y = y1; y <= y2; y++)
+	{
+	  float dy = y - center.y;
+	  float radius = sqrt (dx * dx + dy * dy);
+	  if (radius < width)
+	  {
+		int d = (int) ((patch(x,y) - minIntensity) / quantum);
+		d = d >? 0;
+		d = d <? binsIntensity - 1;
+		int r = (int) (radius / binRadius);
+		result[r * binsIntensity + d] += 1.0;
+	  }
+	  cerr << endl;
+	}
+  }
 
   // Convert to probabilities
 cerr << "time = " << getTimestamp () - startTime << endl;
@@ -222,105 +236,7 @@ DescriptorSpin::rangeMeanDeviation (const Image & image, const PointAffine & poi
 cerr << "Using average: " << minIntensity << " " << quantum << endl;
 }
 
-// WARNING!!! -- This function does not work right under any level of
-// optimization unless "-ffloat-store" is also specified.  It seems that
-// superfluous precision causes some ratios a / b == 1 to actually be
-// slightly larger than 1, causing asin () to return a nan.
 void
 DescriptorSpin::doBinning (const Image & image, const PointAffine & point, int x1, int y1, int x2, int y2, float width, float minIntensity, float quantum, float binRadius, Vector<float> & result)
 {
-  ImageOf<float> that (image);
-  result.resize (binsRadial * binsIntensity);
-  result.clear ();
-
-  for (int x = x1; x <= x2; x++)
-  {
-	float dx = x - point.x;
-	float left  = dx - 0.5;  // Positions of edges of current pixel
-	float right = dx + 0.5;
-	float signLeft = left >= 0 ? 1.0 : -1.0;
-	float signRight = right > 0 ? 1.0 : -1.0;
-	float left2 = left * left;
-	float right2 = right * right;
-
-	for (int y = y1; y <= y2; y++)
-	{
-	  float dy = y - point.y;
-	  float radius = sqrt (dx * dx + dy * dy);
-	  if (radius < width)
-	  {
-		int d = (int) ((that (x, y) - minIntensity) / quantum);
-		d = d >? 0;
-		d = d <? binsIntensity - 1;
-
-		float modRadius = fmod (radius, binRadius);
-		if (modRadius > hsqrt2  &&  modRadius < binRadius - hsqrt2)
-		{
-		  int r = (int) (radius / binRadius);
-		  result[r * binsIntensity + d] += 1.0;
-		  continue;
-		}
-
-		float top    = dy - 0.5;
-		float bottom = dy + 0.5;
-		int r = (int) ((radius - hsqrt2) / binRadius);
-		r = r >? 0;
-		float remaining = 1.0;  // Amount of current pixel not yet allocated
-		for (; r < binsRadial  &&  remaining > 1e-6; r++)
-		{
-		  float area = 0;
-		  float r1 = (r + 1) * binRadius;
-		  float r2 = r1 * r1;
-
-		  // Subtract area between curve and left edge of pixel
-		  float w = r2 - left2;
-		  if (w > 0)
-		  {
-			w = sqrt (w);
-			float a = top >? -w;
-			float b = bottom <? w;
-			if (a < b)
-			{
-			  area +=   (  ((b / 2) * sqrt (r2 - b * b >? 0) + (r2 / 2) * asin (b / r1))
-					     - ((a / 2) * sqrt (r2 - a * a >? 0) + (r2 / 2) * asin (a / r1)))
-				      * signLeft
-				      - left * (b - a);
-			}
-		  }
-
-		  // Add area between curve and right edge of pixel
-		  w = r2 - right2;
-		  if (w > 0)
-		  {
-			w = sqrt (w);
-			float a = top >? -w;
-			float b = bottom <? w;
-			if (a < b)
-			{
-			  area -=   (  ((b / 2) * sqrt (r2 - b * b >? 0) + (r2 / 2) * asin (b / r1))
-						 - ((a / 2) * sqrt (r2 - a * a >? 0) + (r2 / 2) * asin (a / r1)))
-				      * signRight
-				      - right * (b - a);
-			}
-		  }
-
-		  // If necessary, add area between curve and center line
-		  if (left < 0  &&  right > 0)
-		  {
-			float a = top >? -r1;
-			float b = bottom <? r1;
-			if (a < b)
-			{
-			  area +=   (  ((b / 2) * sqrt (r2 - b * b >? 0) + (r2 / 2) * asin (b / r1))
-					     - ((a / 2) * sqrt (r2 - a * a >? 0) + (r2 / 2) * asin (a / r1)))
-				      * 2.0;
-			}
-		  }
-
-		  result[r * binsIntensity + d] += area - (1.0 - remaining);
-		  remaining = 1.0 - area;
-		}
-	  }
-	}
-  }
 }
