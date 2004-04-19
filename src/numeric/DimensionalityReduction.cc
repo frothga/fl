abfcc1263718c56563df3e31faf97f642222cf45
rothgang <none@none>
2004-04-19 17:10:35 -0600

    Provide default implementations for analyze().  Instantiate the productMap for DimensionalityReduction.

--- src/numeric/DimensionalityReduction.cc
+++ src/numeric/DimensionalityReduction.cc
@@ -1,9 +1,27 @@
 #include "fl/reduce.h"
+#include "fl/factory.h"
 
 
 using namespace std;
 using namespace fl;
 
+
+// DimensionalityReduction ----------------------------------------------------
+
+Factory<DimensionalityReduction>::productMap Factory<DimensionalityReduction>::products;
+
+void
+DimensionalityReduction::analyze (const vector< Vector<float> > & data)
+{
+  vector<int> classAssignments (data.size (), 0);
+  analyze (data, classAssignments);
+}
+
+void
+DimensionalityReduction::analyze (const vector< Vector<float> > & data, const vector<int> & classAssignments)
+{
+  analyze (data);
+}
 
 void
 DimensionalityReduction::read (istream & stream)
