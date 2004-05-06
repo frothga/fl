2db9567a361226b4abd4406f56af04bf1fd3d0bc
rothgang <none@none>
2004-05-06 16:25:11 -0600

    Add functions to force use of "f" forms of math functions for floats rather than converting up to double.

---
+++ include/fl/math.h
@@ -0,0 +1,27 @@
+#ifndef fl_math_h
+#define fl_math_h
+
+
+#include <math.h>
+
+
+inline float
+fabs (float a)
+{
+  return fabsf (a);
+}
+
+inline float
+sqrt (float a)
+{
+  return sqrtf (a);
+}
+
+inline float
+pow (float a, float b)
+{
+  return powf (a, b);
+}
+
+
+#endif
