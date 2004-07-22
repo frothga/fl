6126db27651544e89cd65e17d0cac7bdf247f3d5
rothgang <none@none>
2004-07-22 15:18:12 -0600

    Probe for double-buffering first.  Workaround (?) for newer XFree86 servers.

--- src/X/glx.cc
+++ src/X/glx.cc
@@ -16,45 +16,37 @@ fl::GLXContext::GLXContext (fl::Screen * screen)
   this->screen = screen;
 
   // Determine a visual
-  doubleBuffer = false;
-  int attributesSingleBuffer[] =
+  doubleBuffer = true;
+  int attributesDoubleBuffer[] =
   {
 	GLX_RGBA,
+	GLX_DOUBLEBUFFER,
 	GLX_RED_SIZE, 1,
 	GLX_GREEN_SIZE, 1,
 	GLX_BLUE_SIZE, 1,
 	None
   };
-  XVisualInfo * vinfo = glXChooseVisual (screen->display->display, screen->number, attributesSingleBuffer);
-cerr << "vinfo = " << vinfo << endl;
+  XVisualInfo * vinfo = glXChooseVisual (screen->display->display, screen->number, attributesDoubleBuffer);
   if (! vinfo)
   {
-	doubleBuffer = true;
-	int attributesDoubleBuffer[] =
+	doubleBuffer = false;
+	int attributesSingleBuffer[] =
 	{
 	  GLX_RGBA,
-	  GLX_DOUBLEBUFFER,
 	  GLX_RED_SIZE, 1,
 	  GLX_GREEN_SIZE, 1,
 	  GLX_BLUE_SIZE, 1,
 	  None
 	};
-	vinfo = glXChooseVisual (screen->display->display, screen->number, attributesDoubleBuffer);
+	vinfo = glXChooseVisual (screen->display->display, screen->number, attributesSingleBuffer);
 	if (! vinfo)
 	{
 	  throw "Can't find a visual";
 	}
   }
-cerr << "vinfo = " << vinfo << endl;
-cerr << "  visual = " << vinfo->visual << endl;
-cerr << "      id = " << hex << vinfo->visualid << dec << endl;
-cerr << "   depth = " << vinfo->depth << endl;
-cerr << "  colormap_size = " << vinfo->colormap_size << endl;
-cerr << "  bits_per_rgb = " << vinfo->bits_per_rgb << endl;
 
   // Finish creating context
   context = glXCreateContext (screen->display->display, vinfo, NULL, true);
-cerr << "context = " << context << endl;
 }
 
 fl::GLXContext::~GLXContext ()
@@ -74,7 +66,6 @@ fl::GLXContext::isDirect () const
 void
 fl::GLXDrawable::makeCurrent (fl::GLXContext & context) const
 {
-  cerr << "make current: " << screen->display->display << " " << hex << id << dec << " " << context.context << endl;
   glXMakeCurrent (screen->display->display, id, context.context);
 }
 
