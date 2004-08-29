1573f267dbee45a266891db90eafce7dd71950a3
rothgang <none@none>
2004-08-29 16:01:58 -0600

    Simpler code for adding data of one collection to the other.

--- src/image/Interest.cc
+++ src/image/Interest.cc
@@ -12,13 +12,7 @@ InterestOperator::run (const Image & image, vector<PointInterest> & result)
 {
   multiset<PointInterest> temp;
   run (image, temp);
-
-  result.resize (temp.size ());
-  multiset<PointInterest>::iterator it;
-  int i = 0;
-  for (it = temp.begin (); it != temp.end (); it++)
-  {
-	result[i++] = *it;
-  }
+  result.clear ();
+  result.insert (result.end (), temp.begin (), temp.end ());
 }
 
