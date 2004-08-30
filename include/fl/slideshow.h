1ba5cb2418d04a7ba7fb4cd794ea36489c8b41da
rothgang <none@none>
2004-08-30 00:03:26 -0600

    Update comments to Doxygen format.
    Use pthreads to handle waiting, rather than spinning.
    
    Remove dead member variables.

--- include/fl/slideshow.h
+++ include/fl/slideshow.h
@@ -11,34 +11,35 @@ namespace fl
   class SlideShow : public Window
   {
   public:
-	SlideShow ();  // Figure out everything automatically
+	SlideShow ();
 	~SlideShow ();
 
 	virtual bool processEvent (XEvent & event);
 
-	void show (const Image & image, int centerX = 0, int centerY = 0);  // Start displaying image.  Ensure that the point (centerX, centerY) is in the displayable area.
+	void show (const Image & image, int centerX = 0, int centerY = 0);  ///< Start displaying image.  Ensure that the point (centerX, centerY) is in the displayable area.
 	void waitForClick ();
+	void stopWaiting ();  ///< Releases all threads waiting on this window.
 	void paint ();
 
-	//int                depth;
 	fl::Visual *       visual;
-	//PixelFormatRGBBits format;
 	fl::Colormap *     colormap;
 	fl::GC *           gc;
-	bool               waitingForClick;
 	Image              image;
 	XImage *           ximage;
 	pthread_mutex_t    mutexImage;
 	Atom               WM_DELETE_WINDOW;
-	Atom               WM_PROTOCOLS;  // For some reason, this isn't defined in Xatom.h
+	Atom               WM_PROTOCOLS;  ///< For some reason, this isn't defined in Xatom.h
 
-	bool               modeDrag;  // Indicates that there was motion between button down and button up
-	int                lastX;  // Where the last button event occurred
+	bool               modeDrag;  ///< Indicates that there was motion between button down and button up
+	int                lastX;  ///< Where the last button event occurred
 	int                lastY;
-	int                offsetX; // Position in image where window starts
+	int                offsetX; ///< Position in image where window starts
 	int                offsetY;
-	int                width;   // Current size of window
+	int                width;   ///< Current size of window
 	int                height;
+
+	pthread_mutex_t    waitingMutex;
+	pthread_cond_t     waitingCondition;
   };
 }
 
