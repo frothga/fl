cdb6e3434e0034d560513ea3eb40dc0da817c642
rothgang <none@none>
2005-01-12 04:57:58 -0700

    Add functions (sqrt, pow) to the std namespace to ensure that all common numeric types are covered.
    Adapt to environments (such as cygwin) that lack a definition of INFINITY, NAN, and fpclassify.

--- include/fl/math.h
+++ include/fl/math.h
@@ -2,25 +2,59 @@
 #define fl_math_h
 
 
-#include <math.h>
+#include <cmath>
 
 
-inline float
-fabs (float a)
+namespace std
 {
-  return fabsf (a);
+  inline int sqrt (int a)
+  {
+	return (int) floorf (sqrtf ((float) a));
+  }
+
+  inline int pow (int a, int b)
+  {
+	return (int) floor (pow ((double) a, b));
+  }
+
+  inline float pow (int a, float b)
+  {
+	return powf ((float) a, b);
+  }
 }
 
-inline float
-sqrt (float a)
+
+#ifndef INFINITY
+static union
 {
-  return sqrtf (a);
+  unsigned char c[4];
+  float f;
+} fl_infinity = {0, 0, 0x80, 0x7f};
+#define INFINITY fl_infinity.f
+#endif
+
+#ifndef NAN
+static union
+{
+  unsigned char c[4];
+  float f;
+} fl_nan = {0, 0, 0xc0, 0x7f};
+#define NAN fl_nan.f
+#endif
+
+
+inline bool
+issubnormal (float a)
+{
+  return    ((*(unsigned int *) &a) & 0x7F800000) == 0
+	     && ((*(unsigned int *) &a) &   0x7FFFFF) != 0;
 }
 
-inline float
-pow (float a, float b)
+inline bool
+issubnormal (double a)
 {
-  return powf (a, b);
+  return    ((*(unsigned long long *) &a) & 0x7FF0000000000000ll) == 0
+	     && ((*(unsigned long long *) &a) &    0xFFFFFFFFFFFFFll) != 0;
 }
 
 
