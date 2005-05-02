259c3068a7cdae3b89e27d914ef9eeffb236dae7
Fred <none@none>
2005-05-02 00:25:20 -0600

    Change all symbolic constants to upper case, in keeping with C programming conventions.
    Moved alpha channel to LSB in order to be compatible with new convention for get/setRGBA().

--- include/fl/color.h
+++ include/fl/color.h
@@ -14,33 +14,27 @@ for details.
 #define fl_color_h
 
 
-// Note: These #defines conflict with the X header files, and probably others
-// as well.  Maybe a better strategy would be to make all the color constants
-// into global variables, or to make them ALLCAPS, as is traditional for
-// preprocessor contants.
+#define WHITE     0xFFFFFFFF
+#define GRAY100   0xFFFFFFFF
+#define GRAY50    0x808080FF
+#define GRAY0     0x000000FF
+#define BLACK     0x000000FF
 
+#define RED       0xFF0000FF
+#define GREEN     0x00FF00FF
+#define BLUE      0x0000FFFF
 
-#define white     0xFFFFFFFF
-#define gray100   0xFFFFFFFF
-#define gray50    0xFF808080
-#define gray0     0xFF000000
-#define black     0xFF000000
+#define PURPLE    0xFF00FFFF
 
-#define red       0xFFFF0000
-#define green     0xFF00FF00
-#define blue      0xFF0000FF
+#define CYAN100   0x00FFFFFF
+#define CYAN50    0x008080FF
+#define CYAN      CYAN100
 
-#define purple    0xFFFF00FF
-
-#define cyan100   0xFF00FFFF
-#define cyan50    0xFF008080
-#define cyan      cyan100
-
-#define yellow100 0xFFFFFF00
-#define yellow90  0xFFE6E600
-#define yellow75  0xFFC0C000
-#define yellow50  0xFF808000
-#define yellow    yellow90
+#define YELLOW100 0xFFFF00FF
+#define YELLOW90  0xE6E600FF
+#define YELLOW75  0xC0C000FF
+#define YELLOW50  0x808000FF
+#define YELLOW    YELLOW90
 
 
 #endif
