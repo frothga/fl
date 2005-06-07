7180d8942effcae7c74da10f9305d180878859bc
Fred <none@none>
2005-06-07 04:00:04 -0600

    Added MSER and changed interface to pass collections of pointers (allows polymorphism in type of returend interest points).

--- include/fl/interest.h
+++ include/fl/interest.h
@@ -4,6 +4,10 @@ Copyright (c) 2001-2004 Dept. of Computer Science and Beckman Institute,
                         Univ. of Illinois.  All rights reserved.
 Distributed under the UIUC/NCSA Open Source License.  See LICENSE-UIUC
 for details.
+
+
+5/2005 Fred Rothganger -- Added MSER and changed interface to pass collections
+       of pointers.
 */
 
 
@@ -21,11 +25,28 @@ namespace fl
 {
   // General interest operator interface ----------------------------------------
 
+  class InterestPointSet : public std::vector<PointInterest *>
+  {
+  public:
+	InterestPointSet ();
+	~InterestPointSet ();  ///< Destroy the points if we own them.
+
+	void add (const std::multiset<PointInterest> & points);
+
+	bool ownPoints;  ///< true if we are responsible to destroy the points (default value).
+  };
+
   class InterestOperator
   {
   public:
-	virtual void run (const Image & image, std::multiset<PointInterest> & result) = 0;
-	virtual void run (const Image & image, std::vector<PointInterest> & result);  ///< Same as above, just different type of collection for return value.
+	/**
+	   Returns a collection of interest points.
+	   \param result If this class sorts points internally,
+	   then the collection will be in ascending order by weight.  If the
+	   collection already contains entries, then the newly detected points
+	   will be appended to the end.
+	 **/
+	virtual void run (const Image & image, InterestPointSet & result) = 0;
   };
 
 
@@ -36,7 +57,7 @@ namespace fl
   public:
 	InterestHarris (int neighborhood = 5, int maxPoints = 5000, float thresholdFactor = 0.02);
 
-	virtual void run (const Image & image, std::multiset<PointInterest> & result);
+	virtual void run (const Image & image, InterestPointSet & result);
 
 	NonMaxSuppress nms;
 	FilterHarris filter;
@@ -49,7 +70,7 @@ namespace fl
   public:
 	InterestHarrisLaplacian (int maxPoints = 5000, float thresholdFactor = 0.02, float neighborhood = 1, float firstScale = 1, float lastScale = 25, int extraSteps = 20, float stepSize = -1);
 
-	virtual void run (const Image & image, std::multiset<PointInterest> & result);
+	virtual void run (const Image & image, InterestPointSet & result);
 
 	std::vector<FilterHarris> filters;  ///< FilterHarris clearly outperforms FilterHarrisEigen in tests.
 	std::vector<Laplacian> laplacians;
@@ -66,7 +87,7 @@ namespace fl
   public:
 	InterestLaplacian (int maxPoints = 5000, float thresholdFactor = 0.02, float neighborhood = 1, float firstScale = 1, float lastScale = 25, int extraSteps = 20, float stepSize = -1);  ///< neighborhood >= 0 means fixed size (min = 1 pixel); neighborhood < 0 means multiple of scale.
 
-	virtual void run (const Image & image, std::multiset<PointInterest> & result);
+	virtual void run (const Image & image, InterestPointSet & result);
 
 	std::vector<Laplacian> laplacians;
 	int maxPoints;
@@ -86,7 +107,7 @@ namespace fl
   public:
 	InterestHessian (int maxPoints = 5000, float thresholdFactor = 0.02, float neighborhood = 1, float firstScale = 1, float lastScale = 25, int extraSteps = 20, float stepSize = -1);  ///< neighborhood >= 0 means fixed size (min = 1 pixel); neighborhood < 0 means multiple of scale.
 
-	virtual void run (const Image & image, std::multiset<PointInterest> & result);
+	virtual void run (const Image & image, InterestPointSet & result);
 
 	std::vector<FilterHessian> filters;
 	std::vector<Laplacian> laplacians;
@@ -111,7 +132,7 @@ namespace fl
   public:
 	InterestDOG (float firstScale = 1.6f, float lastScale = INFINITY, int extraSteps = 3);  ///< extraSteps gives the number of sub-levels between octaves.
 
-	virtual void run (const Image & image, std::multiset<PointInterest> & result);
+	virtual void run (const Image & image, InterestPointSet & result);
 
 	bool isLocalMax (float value, ImageOf<float> & dog, int x, int y);
 	bool notOnEdge (ImageOf<float> & dog, int x, int y);
@@ -126,6 +147,46 @@ namespace fl
 	bool storePyramid;  ///< Indicates that blurred/resampled images should be stored rather than thrown away.
 	float thresholdEdge;  ///< Gives smallest permissible ratio of det(H) / trace(H)^2, where H is the Hessian of the DoG function on intensity.
 	float thresholdPeak;  ///< Minimum permissible strength of DoG function at a local maximum.
+  };
+
+  class InterestMSER : public InterestOperator
+  {
+  public:
+	InterestMSER (int delta = 1);
+
+	virtual void run (const Image & image, InterestPointSet & result);
+
+	/**
+	   Structure to track meta-data associated with a region (tree) in the
+	   union-find algorithm.
+	 **/
+	struct Node;
+	struct Root
+	{
+	  Root * next;
+	  Root * previous;
+
+	  unsigned char level;  ///< The gray-level where this region was created.
+
+	  int size;  ///< Number of pixels in this tree
+	  int sizes[256];  ///< History of sizes for all gray-levels
+	  float rates[256];  ///< History of changes rates w.r.t. gray-level.  Calculated from sizes[].
+
+	  Node * head;  ///< Start of LIFO linked list of pixels.  IE: points to most recently added pixels.
+	  Node * heads[256];  ///< History of composition of region at each gray-level.  Used to compute affine parameters.
+	};
+
+	struct Node
+	{
+	  Node * parent;
+	  Node * next;
+	  Root * root;  ///< If this is a root node, then this points to the associated metadata;
+	};
+
+	void addGrayLevel (unsigned char level, bool sign, int * lists[], Node * nodes, const int width, const int height, Root * roots, std::vector<PointMSER *> & regions);
+
+	int delta;  ///< Amount of gray-level distance above and below current gray-level to check when computing rate of change in region size.
+	float minScale;  ///< Smallest scale region to admit into resulting list of interest points.  Guards against long skinny structures and structures with too few pixels to be worth noting.
   };
 }
 
