7ac8d5762288e9174b35292d7ca791c5d56928a7
Fred <none@none>
2005-08-06 16:06:54 -0600

    Add header comment regarding previous MSVC fix.

--- src/numeric/KMeans.cc
+++ src/numeric/KMeans.cc
@@ -4,6 +4,10 @@ Copyright (c) 2001-2004 Dept. of Computer Science and Beckman Institute,
                         Univ. of Illinois.  All rights reserved.
 Distributed under the UIUC/NCSA Open Source License.  See LICENSE-UIUC
 for details.
+
+
+4/2005 Fred Rothganger -- Change file modes to binary for compatibility with
+       MSVC.
 */
 
 
