1f0be847795c6e093d99f575f029b56b34368dfc
Fred <none@none>
2005-08-28 21:14:03 -0600

    Utility to measure difference between two video files.

---
+++ src/util/psnr.cc
@@ -0,0 +1,85 @@
+/*
+Author: Fred Rothganger
+Copyright 2005 Sandia Corporation.
+Under the terms of Contract DE-AC04-94AL85000 with Sandia Corporation,
+the U.S. Government retains certain rights in this software.
+*/
+
+
+#include "fl/video.h"
+#include "fl/parms.h"
+#include "fl/math.h"
+
+
+using namespace std;
+using namespace fl;
+
+
+int
+main (int argc, char * argv[])
+{
+  try
+  {
+	Parameters parms (argc, argv);
+	int limit = parms.getInt ("limit", 0);
+
+	if (parms.fileNames.size () != 2)
+	{
+	  cerr << "Usage: " << argv[0] << " [parameters] {video file 1} {video file 2}" << endl;
+	  cerr << "  limit = {max numbers of frames to check} (default = all)" << endl;
+	  return 1;
+	}
+
+	new VideoFileFormatFFMPEG;
+
+	VideoIn vin1 (parms.fileNames[0]);
+	VideoIn vin2 (parms.fileNames[1]);
+
+	double sse = 0;
+	double count = 0;
+
+	Image image1;
+	Image image2;
+	int i = 0;
+	while (vin1.good ()  &&  vin2.good ())
+	{
+	  vin1 >> image1;
+	  vin2 >> image2;
+	  if (! vin1.good ()  ||  ! vin2.good ()) break;
+	  if (image1.width != image2.width  ||  image1.height != image2.height)
+	  {
+		throw "Images not same size";
+	  }
+
+	  ImageOf<float> gray1 = image1 * GrayFloat;
+	  ImageOf<float> gray2 = image2 * GrayFloat;
+
+	  float * f1 = & gray1(0,0);
+	  float * f2 = & gray2(0,0);
+	  int pixels = gray1.width * gray1.height;
+	  float * e = f1 + pixels;
+	  count += pixels;
+	  while (f1 < e)
+	  {
+		float d = *f1++ - *f2++;
+		sse += d * d;
+	  }
+
+	  cerr << ".";
+
+	  i++;
+	  if (limit > 0  &&  i > limit) break;
+	}
+	cerr << endl;
+	double rms = sqrt (sse / count);
+
+	cerr << "psnr = " << 20 * log (1.0 / rms) / log (10.0) << endl;
+  }
+  catch (const char * error)
+  {
+	cerr << "Exception: " << error << endl;
+	return 1;
+  }
+
+  return 0;
+}
