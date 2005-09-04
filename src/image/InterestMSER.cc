1704bb71fbbb35f6418d18bc06dbd74696bd30a2
Fred <none@none>
2005-09-03 23:55:20 -0600

    Add Sandia copyright notice.  This probably needs more work before it is ready for release.

--- src/image/InterestMSER.cc
+++ src/image/InterestMSER.cc
@@ -1,6 +1,9 @@
 /*
 Author: Fred Rothganger
 Created 5/31/2005
+Copyright 2005 Sandia Corporation.
+Under the terms of Contract DE-AC04-94AL85000 with Sandia Corporation,
+the U.S. Government retains certain rights in this software.
 */
 
 
