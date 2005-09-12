1fedc8caa5f55d089edc944f2e7e76e7a3c79d27
Fred <none@none>
2005-09-12 03:22:54 -0600

    Fix Factory template instantiation for GCC 3.4.4.
    Add Sandia copyright notice.  Must add license information before release.

--- src/numeric/ClusterMethod.cc
+++ src/numeric/ClusterMethod.cc
@@ -4,6 +4,12 @@ Copyright (c) 2001-2004 Dept. of Computer Science and Beckman Institute,
                         Univ. of Illinois.  All rights reserved.
 Distributed under the UIUC/NCSA Open Source License.  See LICENSE-UIUC
 for details.
+
+
+08/2005 Fred Rothganger -- Compilability fix for GCC 3.4.4
+Revisions Copyright 2005 Sandia Corporation.
+Under the terms of Contract DE-AC04-94AL85000 with Sandia Corporation,
+the U.S. Government retains certain rights in this software.
 */
 
 
@@ -17,7 +23,8 @@ using namespace std;
 
 // class ClusterMethod --------------------------------------------------------
 
-Factory<ClusterMethod>::productMap Factory<ClusterMethod>::products;
+template class Factory<ClusterMethod>;
+template <> Factory<ClusterMethod>::productMap Factory<ClusterMethod>::products;
 
 void
 ClusterMethod::read (istream & stream)
--- src/numeric/DimensionalityReduction.cc
+++ src/numeric/DimensionalityReduction.cc
@@ -4,6 +4,12 @@ Copyright (c) 2001-2004 Dept. of Computer Science and Beckman Institute,
                         Univ. of Illinois.  All rights reserved.
 Distributed under the UIUC/NCSA Open Source License.  See LICENSE-UIUC
 for details.
+
+
+08/2005 Fred Rothganger -- Compilability fix for GCC 3.4.4
+Revisions Copyright 2005 Sandia Corporation.
+Under the terms of Contract DE-AC04-94AL85000 with Sandia Corporation,
+the U.S. Government retains certain rights in this software.
 */
 
 
@@ -17,7 +23,8 @@ using namespace fl;
 
 // DimensionalityReduction ----------------------------------------------------
 
-Factory<DimensionalityReduction>::productMap Factory<DimensionalityReduction>::products;
+template class Factory<DimensionalityReduction>;
+template <> Factory<DimensionalityReduction>::productMap Factory<DimensionalityReduction>::products;
 
 void
 DimensionalityReduction::analyze (const vector< Vector<float> > & data)
