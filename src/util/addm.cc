cf8eaa17a2f87b2aeeb33de9216f717824ce8431
Fred <none@none>
2005-09-17 16:04:07 -0600

    Add revision history and Sandia copyright notice.  Must add license info before release.

--- src/util/addm.cc
+++ src/util/addm.cc
@@ -4,6 +4,12 @@ Copyright (c) 2001-2004 Dept. of Computer Science and Beckman Institute,
                         Univ. of Illinois.  All rights reserved.
 Distributed under the UIUC/NCSA Open Source License.  See LICENSE-UIUC
 for details.
+
+
+08/2005 Fred Rothganger -- Close files for compatibility with Cygwin.
+Revisions Copyright 2005 Sandia Corporation.
+Under the terms of Contract DE-AC04-94AL85000 with Sandia Corporation,
+the U.S. Government retains certain rights in this software.
 */
 
 
