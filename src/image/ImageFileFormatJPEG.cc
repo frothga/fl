e791ae055a7599b874e4f4fb088bb655762d1b85
Fred <none@none>
2005-10-09 05:18:23 -0600

    Add revision history and Sandia copyright notice.

--- src/image/ImageFileFormatJPEG.cc
+++ src/image/ImageFileFormatJPEG.cc
@@ -4,6 +4,14 @@ Copyright (c) 2001-2004 Dept. of Computer Science and Beckman Institute,
                         Univ. of Illinois.  All rights reserved.
 Distributed under the UIUC/NCSA Open Source License.  See LICENSE-UIUC
 for details.
+
+
+05/2005 Fred Rothganger -- Use new PixelFormat names.
+Revisions Copyright 2005 Sandia Corporation.
+Under the terms of Contract DE-AC04-94AL85000 with Sandia Corporation,
+the U.S. Government retains certain rights in this software.
+Distributed under the GNU Lesser General Public License.  See the file LICENSE
+for details.
 */
 
 
