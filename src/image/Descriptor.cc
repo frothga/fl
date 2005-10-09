b7402d73584bdaa4ae503d0c9121b7c079648749
Fred <none@none>
2005-10-09 05:12:33 -0600

    Compilability fix for GCC 3.4.4: force instantiation of template static member.  Add Sandia copyright notice.

--- src/image/Descriptor.cc
+++ src/image/Descriptor.cc
@@ -4,6 +4,14 @@ Copyright (c) 2001-2004 Dept. of Computer Science and Beckman Institute,
                         Univ. of Illinois.  All rights reserved.
 Distributed under the UIUC/NCSA Open Source License.  See LICENSE-UIUC
 for details.
+
+
+08/2005 Fred Rothganger -- Compilability fix for GCC 3.4.4
+Revisions Copyright 2005 Sandia Corporation.
+Under the terms of Contract DE-AC04-94AL85000 with Sandia Corporation,
+the U.S. Government retains certain rights in this software.
+Distributed under the GNU Lesser General Public License.  See the file LICENSE
+for details.
 */
 
 
@@ -17,6 +25,7 @@ using namespace fl;
 
 // class Descriptor -----------------------------------------------------------
 
+template class Factory<Descriptor>;
 template <> Factory<Descriptor>::productMap Factory<Descriptor>::products;
 
 Descriptor::Descriptor ()
