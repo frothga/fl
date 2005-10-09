b4c2db025fde79de740ba8050491e53753b94dd4
Fred <none@none>
2005-10-09 03:28:17 -0600

    Update revision history and add Sandia copyright notice.

--- include/fl/pi.h
+++ include/fl/pi.h
@@ -6,7 +6,13 @@ Distributed under the UIUC/NCSA Open Source License.  See LICENSE-UIUC
 for details.
 
 
-12/2004 Revised by Fred Rothganger
+12/2004 Fred Rothganger -- Compilability fix for MSVC
+05/2005 Fred Rothganger -- Add TWOPI
+Revisions Copyright 2005 Sandia Corporation.
+Under the terms of Contract DE-AC04-94AL85000 with Sandia Corporation,
+the U.S. Government retains certain rights in this software.
+Distributed under the GNU Lesser General Public License.  See the file LICENSE
+for details.
 */
 
 
