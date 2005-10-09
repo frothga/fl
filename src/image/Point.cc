5eedd16e985769701aece5d0fe860484e6a2b5be
Fred <none@none>
2005-10-09 05:38:31 -0600

    Add Sandia copyright notice.

--- src/image/Point.cc
+++ src/image/Point.cc
@@ -7,6 +7,11 @@ for details.
 
 
 6/2005 Fred Rothganger -- Added PointMSER.
+Revisions Copyright 2005 Sandia Corporation.
+Under the terms of Contract DE-AC04-94AL85000 with Sandia Corporation,
+the U.S. Government retains certain rights in this software.
+Distributed under the GNU Lesser General Public License.  See the file LICENSE
+for details.
 */
 
 
