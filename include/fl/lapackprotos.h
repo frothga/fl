53bfd94c83863e7d24e711fed19e03c8136be9e0
Fred <none@none>
2005-10-08 19:40:02 -0600

    Update revision history and add Sandia copyright notice.
    Add sgelsd_

--- include/fl/lapackprotos.h
+++ include/fl/lapackprotos.h
@@ -4,6 +4,14 @@ Copyright (c) 2001-2004 Dept. of Computer Science and Beckman Institute,
                         Univ. of Illinois.  All rights reserved.
 Distributed under the UIUC/NCSA Open Source License.  See LICENSE-UIUC
 for details.
+
+
+10/2005 Fred Rothganger -- Add sgelsd_
+Revisions Copyright 2005 Sandia Corporation.
+Under the terms of Contract DE-AC04-94AL85000 with Sandia Corporation,
+the U.S. Government retains certain rights in this software.
+Distributed under the GNU Lesser General Public License.  See the file LICENSE
+for details.
 */
 
 
@@ -45,6 +53,21 @@ extern "C"
 				int &         rank,
                 float         work[],
 				const int &   lwork,
+				int &         info);
+
+  void sgelsd_ (const int &   m,
+				const int &   n,
+				const int &   nrhs,
+				float         a[],
+				const int &   lda,
+				float         b[],
+				const int &   ldb,
+				float         s[],
+				const float & rcond,
+				int &         rank,
+                float         work[],
+				const int &   lwork,
+				int           iwork[],
 				int &         info);
 
   void sgesvd_ (const char & jobu,
