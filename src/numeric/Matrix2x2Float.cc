532180385a5af52e00488f6d6a706eb36989fb97
Fred <none@none>
2005-10-13 03:37:34 -0600

    Move geev() out of matrix.h into Matrix2x2.tcc.  Add Sandia copyright notice.

--- src/numeric/Matrix2x2Float.cc
+++ src/numeric/Matrix2x2Float.cc
@@ -4,13 +4,23 @@ Copyright (c) 2001-2004 Dept. of Computer Science and Beckman Institute,
                         Univ. of Illinois.  All rights reserved.
 Distributed under the UIUC/NCSA Open Source License.  See LICENSE-UIUC
 for details.
+
+
+09/2005 Fred Rothganger -- Moved geev() out of matrix.h into Matrix2x2.tcc
+Revisions Copyright 2005 Sandia Corporation.
+Under the terms of Contract DE-AC04-94AL85000 with Sandia Corporation,
+the U.S. Government retains certain rights in this software.
+Distributed under the GNU Lesser General Public License.  See the file LICENSE
+for details.
 */
 
 
 #include "fl/Matrix2x2.tcc"
 
 
-using namespace fl;
-
-
-template class Matrix2x2<float>;
+namespace fl
+{
+  template class Matrix2x2<float>;
+  template void geev (const Matrix2x2<float> & A, Matrix<float> & eigenvalues);
+  template void geev (const Matrix2x2<float> & A, Matrix<std::complex<float> > & eigenvalues);
+}
