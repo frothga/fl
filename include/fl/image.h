178b43933d89392e95073b6c26de60e79e86a3de
Fred <none@none>
2006-02-25 23:17:54 -0700

    Add note to revision log.

--- include/fl/image.h
+++ include/fl/image.h
@@ -13,6 +13,9 @@ Under the terms of Contract DE-AC04-94AL85000 with Sandia Corporation,
 the U.S. Government retains certain rights in this software.
 Distributed under the GNU Lesser General Public License.  See the file LICENSE
 for details.
+
+
+02/2006 Fred Rothganger -- Change Image structure to use PixelBuffer.
 */
 
 
