03ff2380df76c7aaef0d0bec353336d412c67e9b
Fred <none@none>
2006-03-20 05:40:07 -0600

    Get rid of hint.  Instead, follow the paradigm of returing an image with no conversion.

--- src/image/Video.cc
+++ src/image/Video.cc
@@ -19,13 +19,13 @@ using namespace fl;
 
 // class VideoIn ----------------------------------------------------------------
 
-VideoIn::VideoIn (const string & fileName, const fl::PixelFormat & hint)
+VideoIn::VideoIn (const string & fileName)
 {
   file = 0;
   VideoFileFormat * format = VideoFileFormat::find (fileName);
   if (format)
   {
-	file = format->openInput (fileName, hint);
+	file = format->openInput (fileName);
   }
 }
 
