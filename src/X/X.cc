a6234bc05478d77aa7a136fb89bd9eee511ce62d
Fred <none@none>
2007-03-23 11:09:26 -0600

    Correct which revisions are under Sandia copyright.

--- src/X/X.cc
+++ src/X/X.cc
@@ -6,8 +6,8 @@ Distributed under the UIUC/NCSA Open Source License.  See the file LICENSE
 for details.
 
 
-Revisions 1.3 thru 1.6 Copyright 2005 Sandia Corporation.
-Revisions 1.7 thru 1.9 Copyright 2007 Sandia Corporation.
+Revisions 1.3 and 1.5   Copyright 2005 Sandia Corporation.
+Revisions 1.7 thru 1.10 Copyright 2007 Sandia Corporation.
 Under the terms of Contract DE-AC04-94AL85000 with Sandia Corporation,
 the U.S. Government retains certain rights in this software.
 Distributed under the GNU Lesser General Public License.  See the file LICENSE
@@ -16,6 +16,9 @@ for details.
 
 -------------------------------------------------------------------------------
 $Log$
+Revision 1.10  2007/03/23 11:09:26  Fred
+Correct which revisions are under Sandia copyright.
+
 Revision 1.9  2007/02/25 14:46:37  Fred
 Use CVS Log to generate revision history.
 
