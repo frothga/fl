2c86482f97d9f625e5dacb73c8f0274a3802532a
Fred <none@none>
2007-03-25 14:43:55 -0600

    Wrap main code in try block.  Add more image formats.

--- src/util/xv.cc
+++ src/util/xv.cc
@@ -3,6 +3,7 @@ Author: Fred Rothganger
 
 
 Revisions 1.1 and 1.2 Copyright 2005 Sandia Corporation.
+Revisions 1.3 and 1.4 Copyright 2007 Sandia Corporation.
 Under the terms of Contract DE-AC04-94AL85000 with Sandia Corporation,
 the U.S. Government retains certain rights in this software.
 Distributed under the GNU Lesser General Public License.  See the file LICENSE
@@ -11,6 +12,9 @@ for details.
 
 -------------------------------------------------------------------------------
 $Log$
+Revision 1.4  2007/03/25 14:43:55  Fred
+Wrap main code in try block.  Add more image formats.
+
 Revision 1.3  2007/03/23 11:06:58  Fred
 Use CVS Log to generate revision history.
 
@@ -34,25 +38,35 @@ using namespace std;
 int
 main (int argc, char * argv[])
 {
-  if (argc < 2)
+  try
   {
-	cerr << "Usage: " << argv[0] << " {image file names}" << endl;
-	return 1;
+	if (argc < 2)
+	{
+	  cerr << "Usage: " << argv[0] << " {image file names}" << endl;
+	  return 1;
+	}
+
+
+	new ImageFileFormatPGM;
+	new ImageFileFormatJPEG;
+	new ImageFileFormatTIFF;
+	new ImageFileFormatMatlab;
+	new ImageFileFormatNITF;
+
+
+	SlideShow window;
+
+	for (int i = 1; i < argc; i++)
+	{
+	  Image disp (argv[i]);
+	  window.show (disp);
+	  window.waitForClick ();
+	}
   }
-
-
-  new ImageFileFormatPGM;
-  new ImageFileFormatJPEG;
-  new ImageFileFormatTIFF;
-
-
-  SlideShow window;
-
-  for (int i = 1; i < argc; i++)
+  catch (const char * message)
   {
-	Image disp (argv[i]);
-	window.show (disp);
-	window.waitForClick ();
+	cerr << "Exception: " << message << endl;
+	return 1;
   }
 
   return 0;
