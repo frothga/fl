605654e1c4db41853a436def84cceda56425a01d
frothga@sandia.gov <frothga@sandia.gov>
2008-07-30 00:01:40 -0600

    Add util/thumb.cc to source control.

---
+++ src/util/thumb.cc
@@ -0,0 +1,40 @@
+#include "fl/video.h"
+#include "fl/parms.h"
+#include "fl/convolve.h"
+
+#include <stdlib.h>
+#include <stdio.h>
+
+
+using namespace std;
+using namespace fl;
+
+
+int
+main (int argc, char * argv[])
+{
+  Parameters parms (argc, argv);
+  float size = parms.getFloat ("size", 64);
+
+  new ImageFileFormatJPEG;
+  new VideoFileFormatFFMPEG;
+
+  for (int i = 0; i < parms.fileNames.size (); i++)
+  {
+	string fileName = parms.fileNames[i];
+	string stem = fileName.substr (0, fileName.find_last_of ('.'));
+	cerr << fileName << endl;
+
+	VideoIn vin (fileName);
+	Image frame;
+	vin >> frame;
+
+	double ratio = size / frame.height;
+	TransformGauss small (ratio, ratio);
+	frame = frame * small;
+
+	frame.write (stem + ".jpg", "jpeg");
+  }
+
+  return 0;
+}
