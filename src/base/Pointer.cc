847e41982e8fdf92efe6887ce3f3542452429597
frothga@sandia.gov <frothga@sandia.gov>
2009-10-26 16:01:39 -0600

    Start using "2010" as a placeholder for a future copyright assertion on work subsequent to the copyright assertion approved in 2009.

--- src/base/Pointer.cc
+++ src/base/Pointer.cc
@@ -4,7 +4,7 @@ Created 10/19/2009 to support thread-safe Pointer implementation on generic
 systems.
 
 
-Copyright 2009 Sandia Corporation.
+Copyright 2010 Sandia Corporation.
 Under the terms of Contract DE-AC04-94AL85000 with Sandia Corporation,
 the U.S. Government retains certain rights in this software.
 Distributed under the GNU Lesser General Public License.  See the file LICENSE
